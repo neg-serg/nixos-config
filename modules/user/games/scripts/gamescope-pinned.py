040b5da8e86e58d2e69d86c2a762efc1e9dce088
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-02 07:25:37 +0300

    [games] Extract Python scripts to separate files and add tests

---
+++ modules/user/games/scripts/gamescope-pinned.py
@@ -0,0 +1,17 @@
+import os
+import shlex
+import subprocess
+import sys
+
+GAMESCOPE = "gamescope"
+GAME_RUN = "game-run"
+
+flags = os.environ.get("GAMESCOPE_FLAGS", "-f --adaptive-sync")
+
+cmd = (
+    [GAME_RUN, GAMESCOPE]
+    + shlex.split(flags)
+    + ["--"]
+    + sys.argv[1:]
+)
+raise SystemExit(subprocess.call(cmd))
