f4572b7fdc1490be835f341ddcba07bdfbfeb05b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-02 22:23:00 +0300

    [fix] games: Replace proton-ge-custom with proton-ge-bin

--- modules/user/games/default.nix
+++ modules/user/games/default.nix
@@ -178,7 +178,7 @@ in {
         remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
         localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
         # Add Proton-GE for better compatibility/perf in some titles
-        extraCompatPackages = [pkgs.proton-ge-custom];
+        extraCompatPackages = [pkgs.proton-ge-bin];
       };
 
       gamescope = {
