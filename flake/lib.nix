2e7b5fd1d5611f69315782936aa214333097f966
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-04 17:58:23 +0300

    [hypr] fix build and plugin compatibility

--- flake/lib.nix
+++ flake/lib.nix
@@ -6,51 +6,9 @@
   inherit (nixpkgs) lib;
 
   hyprlandOverlay = system: (_: prev: let
-    esc = lib.escapeShellArg;
-    hyprInfo = inputs.hyprland.sourceInfo or {};
-    hyprRev = hyprInfo.rev or "unknown";
-    hyprRef = hyprInfo.ref or "";
-    hyprBranch =
-      if hyprRef != ""
-      then hyprRef
-      else hyprRev;
-    hyprTag =
-      if (builtins.match "^v[0-9].*" hyprRef) != null
-      then hyprRef
-      else "";
-    hyprTagOrBranch =
-      if hyprTag != ""
-      then hyprTag
-      else hyprBranch;
-    hyprCommits = builtins.toString (hyprInfo.revCount or 0);
-    hyprDate =
-      hyprInfo.lastModifiedDate
-      or (
-        if hyprInfo ? lastModified
-        then "unix:${builtins.toString hyprInfo.lastModified}"
-        else "unknown"
-      );
-    hyprMessage =
-      if hyprTag != ""
-      then "Release ${hyprTag}"
-      else "Flake build ${builtins.substring 0 7 hyprRev}";
     hyprlandBase =
       inputs.hyprland.packages.${system}.hyprland.override {wrapRuntimeDeps = false;};
-    hyprland = hyprlandBase.overrideAttrs (old: {
-      postPatch =
-        (old.postPatch or "")
-        + ''
-          rm -f src/version.h
-          HASH=${esc hyprRev} \
-            BRANCH=${esc hyprBranch} \
-            MESSAGE=${esc hyprMessage} \
-            DATE=${esc hyprDate} \
-            DIRTY= \
-            TAG=${esc hyprTagOrBranch} \
-            COMMITS=${esc hyprCommits} \
-            ./scripts/generateVersion.sh
-        '';
-    });
+    hyprland = hyprlandBase;
   in {
     inherit hyprland;
     inherit (inputs.xdg-desktop-portal-hyprland.packages.${system}) xdg-desktop-portal-hyprland;
@@ -58,6 +16,9 @@
       prev.hyprlandPlugins
       // {
         hy3 = inputs.hy3.packages.${system}.hy3;
+        hyprexpo = prev.hyprlandPlugins.hyprexpo.overrideAttrs (old: {
+          buildInputs = [hyprland] ++ (lib.filter (i: i.pname or "" != "hyprland") old.buildInputs);
+        });
       };
   });
 
