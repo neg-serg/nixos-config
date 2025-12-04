082c1b5cedb1143592231e855e9f5e332c099bbe
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-04 07:35:02 +0300

    [hyprland] refactor config and add uwsm support

--- home/modules/user/envs/zshenv-extra.sh
+++ home/modules/user/envs/zshenv-extra.sh
@@ -4,6 +4,6 @@ if [[ ! -L ${HOME}/tmp ]]; then
   tmp_loc=$(mktemp -d)
   ln -fs "${tmp_loc}" "${HOME}/tmp"
 fi
-# if command -v uwsm > /dev/null && uwsm check may-start; then
-#   exec uwsm start default
-# fi
+if command -v uwsm > /dev/null && uwsm check may-start; then
+  exec uwsm start default
+fi
