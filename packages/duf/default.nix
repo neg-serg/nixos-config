ea844667b74fcbd284907e79d3dda7d9a228b485
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-04 09:57:25 +0300

    [packages] update duf with modern progress bar

--- packages/duf/default.nix
+++ packages/duf/default.nix
@@ -10,8 +10,8 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "neg-serg";
     repo = "duf";
-    rev = "83201f3f8bbc6b180d53523397755f2084a31ac8";
-    hash = "sha256-WahArld9hdz349+mJQnzMappisJzENRdKFFN0ZAsPBY=";
+    rev = "71c5a20c53b775cf9f153384f00c822a370a723c";
+    hash = "sha256-/FIKIijqNhOODohKBFcX7xlTyLS2wdK83g5GdQ0v148=";
   };
 
   vendorHash = "sha256-mCOP6R072dmJBHN8c7ae8l7yN1O25FDLIgRGUSWUn2E=";
