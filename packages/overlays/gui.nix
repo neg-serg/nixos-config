47d57816344cfd4f6fef8f7080ba7a14b9d36e46
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-05 01:37:52 +0300

    [pyprland] switch to official flake input

--- packages/overlays/gui.nix
+++ packages/overlays/gui.nix
@@ -88,11 +88,5 @@ in {
 
   flight-gtk-theme = callPkg (inputs.self + "/packages/flight-gtk-theme") {};
 
-  pyprland = prev.pyprland.overrideAttrs (old: {
-    postPatch =
-      (old.postPatch or "")
-      + ''
-        sed -i -E 's/^([[:space:]]*)split_version = \[int\(i\) for i in version_str\.split\("\."\)\[:3\]\]/\1try: split_version = [int(i) for i in version_str.split(".")[:3]]\n\1except ValueError: split_version = [0, 0, 0]/' pyprland/plugins/pyprland.py
-      '';
-  });
+  pyprland = inputs.pyprland.packages.${prev.system}.pyprland;
 }
