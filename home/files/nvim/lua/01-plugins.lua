a55c1fd479fff279dc3e11154309339a9d7a237e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-07 10:45:41 +0300

    [dev/editor] Re-enable Lazy docs when writable

--- home/files/nvim/lua/01-plugins.lua
+++ home/files/nvim/lua/01-plugins.lua
@@ -2,11 +2,15 @@ local ok, nixCats = pcall(require, "nixCats")
 local lazy
 local lockfile = vim.fn.stdpath("state") .. "/lazy-lock.json"
 
--- Disable helptags generation (tries to write into nix store paths)
 local plugin_tasks_ok, plugin_tasks = pcall(require, "lazy.manage.task.plugin")
 if plugin_tasks_ok and plugin_tasks.docs then
-    plugin_tasks.docs.run = function()
-        return
+    local orig_docs_run = plugin_tasks.docs.run
+    plugin_tasks.docs.run = function(self)
+        local docs = self.plugin.dir .. "/doc"
+        if docs:match("^/nix/store/") then
+            return
+        end
+        return orig_docs_run(self)
     end
 end
 if ok and nixCats.lazy then
