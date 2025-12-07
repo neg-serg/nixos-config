0eb70bb50d7898cca31acc86999a1f698af8a167
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-07 10:37:21 +0300

    [dev/editor] Wire nvf to user init and disable lazy helptags

--- home/files/nvim/lua/01-plugins.lua
+++ home/files/nvim/lua/01-plugins.lua
@@ -1,8 +1,18 @@
 local ok, nixCats = pcall(require, "nixCats")
 local lazy
+local lockfile = vim.fn.stdpath("state") .. "/lazy-lock.json"
+
+-- Disable helptags generation (tries to write into nix store paths)
+local plugin_tasks_ok, plugin_tasks = pcall(require, "lazy.manage.task.plugin")
+if plugin_tasks_ok and plugin_tasks.docs then
+    plugin_tasks.docs.run = function()
+        return
+    end
+end
 if ok and nixCats.lazy then
     lazy = nixCats.lazy
     lazy.setup({
+        lockfile = lockfile,
         defaults = { lazy = false },
         install = { colorscheme = { "neg" } },
         ui = { icons = { ft = "", lazy = "󰂠 ", loaded = "", not_loaded = "" } },
@@ -27,6 +37,7 @@ else
     vim.opt.rtp:prepend(lazypath)
     lazy = require("lazy")
     lazy.setup({
+        lockfile = lockfile,
         spec = { { import = "plugins" } },
         defaults = { lazy = false },
         install = { colorscheme = { "neg" } },
