f1980390e91937cd2f59d573adeb33a5935ca462
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-08 16:12:19 +0300

    [nix/clblast] Drop obsolete CMake patch

--- modules/nix/clblast.nix
+++ modules/nix/clblast.nix
@@ -2,12 +2,7 @@ _: {
   nixpkgs.overlays = [
     (_: prev: {
       clblast = prev.clblast.overrideAttrs (old: {
-        # Bump CLBlast's CMake floor to match current CMake policy requirements.
-        patches =
-          (old.patches or [])
-          ++ [
-            ./patches/clblast-min-cmake-3_5.patch
-          ];
+        patches = old.patches or [];
       });
     })
   ];
