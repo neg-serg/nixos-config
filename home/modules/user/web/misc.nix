5e2c18b384b1228ff40bd8a1f271e40afe0928dc
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-09 12:55:31 +0300

    [web] add package comment for neonmodem

--- home/modules/user/web/misc.nix
+++ home/modules/user/web/misc.nix
@@ -7,5 +7,5 @@
 lib.mkIf (config.features.web.enable && config.features.web.tools.enable) {
   # Tools moved to system-wide CLI package set (modules/cli/pkgs.nix) to keep
   # them available outside Home Manager.
-  home.packages = [pkgs.neonmodem];
+  home.packages = [pkgs.neonmodem]; # TUI for Lemmy/Kbin
 }
