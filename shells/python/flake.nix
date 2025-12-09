3ff7491a307930cc4321a2f6c2ef3b51616737d9
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-09 09:20:48 +0300

    [shells] add python devshell

---
+++ shells/python/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Nix shell for Tarantool";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = [
+          (pkgs.python311.withPackages (python-pkgs: [
+            python-pkgs.pip
+            python-pkgs.numpy
+            python-pkgs.graphviz
+            python-pkgs.sympy
+            python-pkgs.scipy
+            python-pkgs.pandas
+            python-pkgs.matplotlib
+            python-pkgs.virtualenv
+            python-pkgs.jupyterlab
+          ]))
+        ];
+
+        # See https://github.com/NixOS/nixpkgs/issues/18995
+        hardeningDisable = ["fortify"];
+      };
+    });
+}
