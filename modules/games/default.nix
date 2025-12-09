5fabfb5a3743babc36e7f23f6567010298d9648e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-09 08:51:43 +0300

    [games] install dualsensectl

--- modules/games/default.nix
+++ modules/games/default.nix
@@ -1,4 +1,4 @@
-_: {
+{pkgs, ...}: {
   boot.kernel.sysctl = {
     "fs.file-max" = 524288;
     "kernel.sched_cfs_bandwidth_slice_us" = 3000; # better settings for cfs
@@ -6,9 +6,13 @@ _: {
     "vm.max_map_count" = 16777216; # need for some games
     "vm.swappiness" = 10; # avoid swapping under load on 64GB RAM
   };
-  # environment.systemPackages = [
-  #   (inputs.nix-gaming.packages.${hostPlatform.system}.star-citizen.override {
-  #     location = "$HOME/games/star-citizen";
-  #   })
-  # ];
+
+  services.udev.packages = [pkgs.dualsensectl];
+
+  environment.systemPackages = [
+    pkgs.dualsensectl
+    # (inputs.nix-gaming.packages.${hostPlatform.system}.star-citizen.override {
+    #   location = "$HOME/games/star-citizen";
+    # })
+  ];
 }
