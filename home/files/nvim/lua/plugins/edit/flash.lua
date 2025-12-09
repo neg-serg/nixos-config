6521d36e8f53913960325f86811fdfecca68f7b2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-09 10:10:23 +0300

    [nvim] enable flash.nvim on Space

--- home/files/nvim/lua/plugins/edit/flash.lua
+++ home/files/nvim/lua/plugins/edit/flash.lua
@@ -1,7 +1,25 @@
 -- ┌───────────────────────────────────────────────────────────────────────────────────┐
 -- │ █▓▒░ folke/flash.nvim                                                             │
 -- └───────────────────────────────────────────────────────────────────────────────────┘
-return {'folke/flash.nvim', event={'VeryLazy'},
-        enabled=false,
-        opts={modes={search={enabled=false}}},
-        keys={}}
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  enabled = true,
+  opts = {
+    modes = {
+      search = {
+        enabled = false,
+      },
+    },
+  },
+  keys = {
+    {
+      "<space>",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+  },
+}
