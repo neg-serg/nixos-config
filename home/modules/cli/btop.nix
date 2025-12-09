646650ff1865991a5eecef45583de8b8b4e4ea79
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-09 10:03:01 +0300

    [cli] btop: enable rocm

--- home/modules/cli/btop.nix
+++ home/modules/cli/btop.nix
@@ -1,6 +1,7 @@
-{
+{pkgs, ...}: {
   programs.btop = {
     enable = true;
+    package = pkgs.btop.override {rocmSupport = true;};
     settings = {
       truecolor = true;
       presets = "cpu:1:default,proc:0:default cpu:0:default,mem:0:default,net:0:default cpu:0:block,net:0:tty";
