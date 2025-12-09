6fbd97bfc5aa5d49c58f7443712fbe7416c28ff0
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-09 10:03:00 +0300

    [web] add neonmodem

--- home/modules/user/web/misc.nix
+++ home/modules/user/web/misc.nix
@@ -1,9 +1,11 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 lib.mkIf (config.features.web.enable && config.features.web.tools.enable) {
   # Tools moved to system-wide CLI package set (modules/cli/pkgs.nix) to keep
   # them available outside Home Manager.
+  home.packages = [pkgs.neonmodem];
 }
