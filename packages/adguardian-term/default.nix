8704bacc4200e0d7ef9820fb33a5121a5bd9d14c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-10 12:06:14 +0300

    [packages] Fix adguardian-term fetch

--- packages/adguardian-term/default.nix
+++ packages/adguardian-term/default.nix
@@ -1,17 +1,23 @@
 {
   lib,
   rustPlatform,
+  fetchFromGitHub,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "adguardian-term";
   version = "1.6.0";
 
-  # Local checkout of https://github.com/Lissy93/AdGuardian-Term
-  src = /home/neg/src/1st_level/AdGuardian-Term;
+  src = fetchFromGitHub {
+    owner = "Lissy93";
+    repo = "AdGuardian-Term";
+    rev = "1.6.0";
+    hash = "sha256-WxrSmCwLnXXs5g/hN3xWE66P5n0RD/L9MJpf5N2iNtY=";
+  };
 
   cargoLock.lockFile = "${src}/Cargo.lock";
-  cargoHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  cargoHash = "sha256-xCfFawZKyeqvrI8BPyr+ycX9jc6v16gMh30cXA81szg=";
 
+  # No extra system dependencies (built with rustls)
   meta = with lib; {
     description = "Terminal-based, real-time traffic monitor for AdGuard Home";
     homepage = "https://github.com/Lissy93/AdGuardian-Term";
