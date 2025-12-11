3361e32c2e09707ea7ae972ac5e1f97adafa0d2b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-11 12:28:25 +0300

    [pipewire] Configure RNNoise noise canceling source

--- home/modules/media/pipewire.nix
+++ home/modules/media/pipewire.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: let
   filesRoot = "${config.neg.hmConfigRoot}/files";
@@ -14,6 +15,42 @@ in
       "pipewire" = {
         source = filesRoot + "/media/pipewire";
         recursive = true;
+      };
+      "pipewire/pipewire.conf.d/99-rnnoise.conf".text = builtins.toJSON {
+        "context.modules" = [
+          {
+            "name" = "libpipewire-module-filter-chain";
+            "args" = {
+              "node.description" = "Noise Canceling source";
+              "media.name" = "Noise Canceling source";
+              "filter.graph" = {
+                "nodes" = [
+                  {
+                    "type" = "ladspa";
+                    "name" = "rnnoise";
+                    "plugin" = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
+                    "label" = "noise_suppressor_stereo";
+                    "control" = {
+                      "VAD Threshold (%)" = 50.0;
+                    };
+                  }
+                ];
+              };
+              "audio.position" = [
+                "FL"
+                "FR"
+              ];
+              "capture.props" = {
+                "node.name" = "effect_input.rnnoise";
+                "node.passive" = true;
+              };
+              "playback.props" = {
+                "node.name" = "effect_output.rnnoise";
+                "media.class" = "Audio/Source";
+              };
+            };
+          }
+        ];
       };
     };
   }
