91dafcef7fac50133841b2750a38be9709f29598
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-11 17:11:43 +0300

    [gui/swayosd] Enable libinput backend and add brightness bindings

--- home/modules/user/gui/swayosd/default.nix
+++ home/modules/user/gui/swayosd/default.nix
@@ -6,4 +6,19 @@
 with lib;
   mkIf (config.features.gui.enable or false) {
     services.swayosd.enable = true;
+
+    systemd.user.services.swayosd-libinput-backend = {
+      Unit = {
+        Description = "SwayOSD LibInput Backend";
+        PartOf = ["graphical-session.target"];
+        After = ["graphical-session.target"];
+      };
+      Service = {
+        ExecStart = "${lib.getExe' pkgs.swayosd "swayosd-libinput-backend"}";
+        Restart = "always";
+      };
+      Install = {
+        WantedBy = ["graphical-session.target"];
+      };
+    };
   }
