c97271aca57bf6e2446451a655891833fdf69ce3
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-11 18:39:54 +0300

    [packages] Add wl-ocr package

--- packages/overlay.nix
+++ packages/overlay.nix
@@ -12,4 +12,5 @@ in
   // {
     neg = (functions.neg or {}) // (tools.neg or {}) // (media.neg or {}) // (dev.neg or {});
     subsonic-tui = final.callPackage ./subsonic-tui {};
+    wl-ocr = final.callPackage ./wl-ocr {};
   }
