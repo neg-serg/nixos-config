220020963b71f46e912b16da27060fb050f5bbb8
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-11 12:20:09 +0300

    [nix] Deprecate legacy system-level nix-index service

--- modules/nix/nixindex.nix
+++ modules/nix/nixindex.nix
@@ -1,19 +1,20 @@
-{pkgs, ...}: {
-  systemd.services.nixindex = {
-    serviceConfig.Type = "oneshot";
-    path = [pkgs.nix-index];
-    script = ''
-      #!/bin/sh
-      ${pkgs.nix-index}/bin/nix-index
-    '';
-  };
+{...}: {
+  # DEPRECATED: Replaced by nix-index-database in home-manager
+  # systemd.services.nixindex = {
+  #   serviceConfig.Type = "oneshot";
+  #   path = [pkgs.nix-index];
+  #   script = ''
+  #     #!/bin/sh
+  #     ${pkgs.nix-index}/bin/nix-index
+  #   '';
+  # };
 
-  systemd.timers.nixindex = {
-    wantedBy = ["timers.target"];
-    partOf = ["nixindex.service"];
-    timerConfig = {
-      OnCalendar = "*-*-* 4:00:00";
-      Unit = "nixindex.service";
-    };
-  };
+  # systemd.timers.nixindex = {
+  #   wantedBy = ["timers.target"];
+  #   partOf = ["nixindex.service"];
+  #   timerConfig = {
+  #     OnCalendar = "*-*-* 4:00:00";
+  #     Unit = "nixindex.service";
+  #   };
+  # };
 }
