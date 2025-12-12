1efdb98d0476d951b95bbf046e9fad60af4e9f1a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-12 06:45:20 +0300

    [modules/hardware] Reorganize packages into logical groups

--- modules/hardware/pkgs.nix
+++ modules/hardware/pkgs.nix
@@ -1,16 +1,27 @@
 {pkgs, ...}: {
   environment.systemPackages = [
+    # -- Bluetooth --
     pkgs.overskride # bluetooth and obex client
     pkgs.bluez-tools # command line bluetooth manager
+
+    # -- Display --
     pkgs.brightnessctl # backlight control helper
+
+    # -- Network --
     pkgs.wirelesstools # iwconfig/ifrename CLI helpers
+
+    # -- System Info --
     pkgs.acpi # ACPI probing utilities
     pkgs.hwinfo # detailed hardware inventory
     pkgs.inxi # summary hardware inspector
     pkgs.lshw # Linux hardware lister
     pkgs.neg.ls_iommu # IOMMU group lister for VFIO planning
+
+    # -- Peripherals --
     pkgs.evhz # HID polling rate monitor
     pkgs.openrgb # peripheral RGB controller
+
+    # -- Embedded / Firmware --
     pkgs.flashrom # firmware flashing CLI
     pkgs.minicom # serial console helper
     pkgs.openocd # on-chip debugger/JTAG helper
