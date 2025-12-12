ca1f81d5b7821310acf42a1ec2d47f245119787c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-12 07:11:39 +0300

    [modules/user] Sort locale and mail packages alphabetically

--- modules/user/locale-pkgs.nix
+++ modules/user/locale-pkgs.nix
@@ -1,10 +1,10 @@
 {pkgs, ...}: {
   environment.systemPackages = [
+    pkgs.enchant # glue/CLI to probe spellchecker providers (hunspell/nuspell backends)
     pkgs.hunspell # classic spellchecker used across desktop apps
     pkgs.hunspellDicts.en_US # English dictionary for hunspell
     pkgs.hunspellDicts.ru_RU # Russian dictionary for hunspell
     pkgs.hyphen # hyphenation patterns for office suites
     pkgs.nuspell # modern spellchecker replacing aspell/hunspell
-    pkgs.enchant # glue/CLI to probe spellchecker providers (hunspell/nuspell backends)
   ];
 }
