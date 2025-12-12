eb243cafa8def2443cccf325a3d27269225248eb
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-12 07:06:43 +0300

    [modules/system] Organize packages into logical groups

--- modules/system/pkgs.nix
+++ modules/system/pkgs.nix
@@ -1,13 +1,20 @@
 {pkgs, ...}: {
   environment.systemPackages = [
+    # -- Disk / Filesystem --
+    pkgs.btrfs-progs # manage and check btrfs filesystems
     pkgs.cryptsetup # stuff for LUKS
+
+    # -- Hardware Info --
     pkgs.dmidecode # extract system/memory/bios info
     pkgs.hw-probe # tool to get information about system
-    pkgs.kexec-tools # tools related to the kexec Linux feature
     pkgs.lm_sensors # sensors
     pkgs.pciutils # manipulate pci devices
-    pkgs.schedtool # CPU scheduling
     pkgs.usbutils # lsusb
-    pkgs.btrfs-progs # manage and check btrfs filesystems
+
+    # -- Kernel / Boot --
+    pkgs.kexec-tools # tools related to the kexec Linux feature
+
+    # -- Scheduling --
+    pkgs.schedtool # CPU scheduling
   ];
 }
