0c7f379c187fd73a606365315858537f0d9853c0
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-12 07:15:28 +0300

    [modules/hardware] Sort QMK and audio DSP packages

--- modules/hardware/audio/dsp/pkgs.nix
+++ modules/hardware/audio/dsp/pkgs.nix
@@ -4,7 +4,7 @@
     pkgs.camilladsp # flexible audio DSP
     pkgs.jamesdsp # PipeWire DSP
     pkgs.lsp-plugins # various audio plugins
-    pkgs.yabridgectl # VST management on Linux
     pkgs.yabridge # VST bridge for Linux
+    pkgs.yabridgectl # VST management on Linux
   ];
 }
--- modules/hardware/qmk/pkgs.nix
+++ modules/hardware/qmk/pkgs.nix
@@ -1,8 +1,8 @@
 {pkgs, ...}: {
   environment.systemPackages = [
     pkgs.keymapviz # visualize QMK keymap.c
-    pkgs.qmk_hid # CLI for interacting with QMK devices over HID
     pkgs.qmk # QMK Firmware helper
+    pkgs.qmk_hid # CLI for interacting with QMK devices over HID
     pkgs.qmk-udev-rules # add QMK udev rules
   ];
 }
