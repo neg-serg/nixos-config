fd259d232643dbec3673334ee887295bffeb4b6a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-12 05:46:39 +0300

    [modules/nix] Enable pipe-operators experimental feature

--- modules/nix/settings.nix
+++ modules/nix/settings.nix
@@ -37,6 +37,7 @@ in {
           "auto-allocate-uids" # allow nix to automatically pick UIDs, rather than creating nixbld* user accounts
           "flakes" # flakes for reprodusability
           "nix-command" # new nix interface
+          "pipe-operators" # pipe operator |> support
         ];
         eval-cache = true;
         allow-import-from-derivation = false;
