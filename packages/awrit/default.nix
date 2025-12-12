9b3f6f477eaef6b811c5be73c784effaf29dae66
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-12 08:24:39 +0300

    [security] Update npm dependencies via maintenance script

--- packages/awrit/default.nix
+++ packages/awrit/default.nix
@@ -25,7 +25,7 @@ in
       hash = "sha256-SUPzVwtMi+5Jq28KzqjXNWJCZkgk9nHelLvHBh42JVo=";
     };
 
-    npmDepsHash = "sha256-MyBqVdKseKbNfet/b+1TU7YJtHaQbTRY4xYgaqVM3ys=";
+    npmDepsHash = "sha256-VhaaVdUoE/kiMywZbTJwAJUj5IsERe9ooF7k+Ti4s7Q=";
 
     patches = [
       ./fix-config-and-version.patch
