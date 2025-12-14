cc95943992fd405f2d91dcba683d7bb132bf7719
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-14 11:46:35 +0300

    [security/sudo] configure colored passprompt (color 25)

--- modules/security/default.nix
+++ modules/security/default.nix
@@ -137,6 +137,7 @@ in {
       package = pkgs.sudo;
       extraConfig = ''
         Defaults timestamp_timeout = 300 # makes sudo ask for password less often
+        Defaults passprompt="\x1b[38;5;25m[sudo] password for %u@%h: \x1b[0m"
       '';
       extraRules = [
         {
