7c9aaf6448ef4a38249bde030c52dba2bb4a7df4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-14 10:22:54 +0300

    [dev/neovim] migrate treesitter parsers and LSPs to nvf extraPackages

--- home/files/nvim/lua/plugins/appearance/nvim-treesitter.lua
+++ home/files/nvim/lua/plugins/appearance/nvim-treesitter.lua
@@ -3,53 +3,9 @@
 -- └───────────────────────────────────────────────────────────────────────────────────┘
 return {
   'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate', -- чтобы автоматически обновлялись парсеры
   config = function()
     require('nvim-treesitter.configs').setup({
-      ensure_installed = {
-        "bash",
-        "caddy",
-        "cmake",
-        "css",
-        "diff",
-        "dockerfile",
-        "gitcommit",
-        "gitignore",
-        "glsl",
-        "go",
-        "gomod",
-        "gosum",
-        "graphql",
-        "html",
-        "http",
-        "ini",
-        "javascript",
-        "json",
-        "just",
-        "kconfig",
-        "lua",
-        "luadoc",
-        "make",
-        "markdown",
-        "markdown_inline",
-        "meson",
-        "ninja",
-        "nix",
-        "php",
-        "python",
-        "query",
-        "regex",
-        "scss",
-        "sql",
-        "svelte",
-        "toml",
-        "vim",
-        "vimdoc",
-        "vue",
-        "wgsl",
-        "xml",
-        "yaml",
-      },
+      ensure_installed = {}, -- managed by Nix 
       highlight = { enable = true }, -- включить подсветку
       indent = { enable = true }, -- умные отступы
     })
--- modules/dev/editor/neovim/pkgs.nix
+++ modules/dev/editor/neovim/pkgs.nix
@@ -1,45 +1,12 @@
 {
   lib,
   config,
-  pkgs,
   ...
 }: let
   devEnabled = config.features.dev.enable or false;
   packages = [
-    pkgs.bash-language-server # Bash LSP for shell scripts
-    pkgs.neovim-remote # nvr helper for external editor integration
-    pkgs.nil # Nix LSP (fast)
-    pkgs.pylyzer # static type analyzer for Python
-    pkgs.pyright # Microsoft Pyright LSP
-    pkgs.ruff # Python formatter/linter CLI + LSP
-    pkgs.clang-tools # clangd/clang-format for C/C++
-    pkgs.lua-language-server # Lua LSP
-    pkgs.hyprls # Hyprland config LSP
-    pkgs.emmet-language-server # Emmet completions for HTML/CSS
-    pkgs.yaml-language-server # YAML LSP
-    pkgs.taplo # TOML LSP/formatter
-    pkgs.marksman # Markdown LSP
-    pkgs.nodePackages_latest.typescript-language-server # TypeScript/JS LSP
-    pkgs.nodePackages_latest.vscode-langservers-extracted # HTML/CSS/JSON LSP bundle
-    pkgs.qt6.qtdeclarative # qmlfmt/qmlcachegen for QML editing
-    pkgs.qt6.qttools # qmlscene/lrelease etc. for QML dev
-    pkgs.just-lsp # LSP for justfiles
-    pkgs.lemminx # XML language server
-    pkgs.awk-language-server # AWK LSP
-    pkgs.autotools-language-server # Autoconf/Automake LSP
-    pkgs.gopls # Go language server
-    pkgs.sqls # SQL language server
-    pkgs.cmake-language-server # CMake LSP
-    pkgs.dhall-lsp-server # Dhall LSP
-    pkgs.docker-compose-language-service # docker-compose schema validation
-    pkgs.dockerfile-language-server # Dockerfile LSP
-    pkgs.dot-language-server # Graphviz DOT LSP
-    pkgs.asm-lsp # Assembly language server
-    pkgs.systemd-language-server # systemd unit LSP
-    pkgs.nginx-language-server # nginx.conf language server
-    pkgs.svls # SystemVerilog LSP
-    pkgs.vhdl-ls # VHDL language server
-    pkgs.zls # Zig language server
+    # Migrated to home/modules/dev/editor/nvf.nix (extraPackages)
+    # Keeping file for potential system-wide tools
   ];
 in {
   config = lib.mkIf devEnabled {
