9bec6b8a5373ec67a98d2c47a43186d7bbeed613
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-15 14:35:34 +0300

    [nvim] fix: restore missing snacks keymaps

--- home/files/nvim/lua/plugins/ui/snacks.lua
+++ home/files/nvim/lua/plugins/ui/snacks.lua
@@ -38,7 +38,8 @@ return {
     words = { enabled = true },
   },
   keys = {
-
+    { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+    { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
     { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
     { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
     { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
