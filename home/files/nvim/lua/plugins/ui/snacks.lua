11c66f6bc3150398e90ca59dd9c4d02ff3c31455
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-15 16:24:15 +0300

    [nvim] fix: restore leader-. to frecency, move scratch to leader-ss

--- home/files/nvim/lua/plugins/ui/snacks.lua
+++ home/files/nvim/lua/plugins/ui/snacks.lua
@@ -42,7 +42,7 @@ return {
     words = { enabled = true },
   },
   keys = {
-    { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+    { "<leader>ss", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
     { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
     { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
     { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
