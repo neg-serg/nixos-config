f46fd5d9f25720f80d7788782f36c738286674d2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-15 18:20:44 +0300

    [theme] chore: temporarily disable stylix to isolate build error

--- home/modules/user/theme/default.nix
+++ home/modules/user/theme/default.nix
@@ -111,7 +111,7 @@ in
         };
       };
 
-      stylix = {
+      _stylix = {
         enable = ! (config.features.devSpeed.enable or false);
         enableReleaseChecks = false;
         autoEnable = false;
