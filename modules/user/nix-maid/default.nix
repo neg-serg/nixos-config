7d8fd630b9869dbae7ce537b01df207de4975303
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-15 19:26:01 +0300

    [maid] feat(mpv): migrate mpv to nix-maid and resolve systemd conflict

--- modules/user/nix-maid/default.nix
+++ modules/user/nix-maid/default.nix
@@ -2,7 +2,15 @@
   imports = [
     inputs.nix-maid.nixosModules.default
     ./git.nix
+    ./mpv.nix
   ];
 
   users.users.neg.maid = {};
+
+  # Workaround: nix-maid hijacks ~/.config/systemd/user, breaking Home Manager.
+  # We redirect nix-maid's config home for the activation service so it creates its systemd link
+  # in a dummy directory, leaving the real one for Home Manager.
+  systemd.user.services.maid-activation = {
+    environment.XDG_CONFIG_HOME = "/home/neg/.cache/maid-systemd-workaround";
+  };
 }
