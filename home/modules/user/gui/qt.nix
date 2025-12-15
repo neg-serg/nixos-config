16b87f2ab2261f771c8564cf0dd083a78a627f7e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-15 18:32:38 +0300

    [maid] fix: restore all modules and add qt compat options

--- home/modules/user/gui/qt.nix
+++ home/modules/user/gui/qt.nix
@@ -6,11 +6,22 @@
 }: let
   qtEnabled = config.features.gui.qt.enable or false;
 in {
+  options.qt.qt5ctSettings = lib.mkOption {
+    type = lib.types.attrs;
+    default = {};
+    description = "Dummy option to fix nix-maid legacy dependency";
+  };
+  options.qt.qt6ctSettings = lib.mkOption {
+    type = lib.types.attrs;
+    default = {};
+    description = "Dummy option to fix nix-maid legacy dependency";
+  };
+
   config = lib.mkIf qtEnabled {
     qt = {
       enable = true;
-      # platformTheme.name = "qt6ct";
-      # style.name = "kvantum";
+      platformTheme.name = "qt6ct";
+      style.name = "kvantum";
     };
 
     home.packages = with pkgs; [
