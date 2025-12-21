f1efa0de7a7322aa801bec60ac73c590036cd9e0
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-22 00:16:24 +0300

    [modules] implement auto-import pattern for modules.nix files

--- modules/roles/modules.nix
+++ modules/roles/modules.nix
@@ -1,9 +1,11 @@
-{
-  imports = [
-    ./workstation.nix
-    ./homelab.nix
-    ./media.nix
-    ./monitoring.nix
-    ./server.nix
-  ];
+# Auto-import all subdirectories (with default.nix) and .nix files (except modules.nix, default.nix)
+let
+  contents = builtins.readDir ./.;
+  isImportable = name: type:
+    if type == "directory"
+    then builtins.pathExists (./. + "/${name}/default.nix")
+    else type == "regular" && name != "modules.nix" && name != "default.nix" && builtins.match ".*\.data\.nix" name == null && builtins.match ".*\.nix" name != null;
+  names = builtins.filter (n: isImportable n contents.${n}) (builtins.attrNames contents);
+in {
+  imports = map (n: ./. + "/${n}") names;
 }
