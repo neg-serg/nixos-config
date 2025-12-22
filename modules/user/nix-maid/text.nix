135e5fcc17952ddfe90fb2b26de46b749013806d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-22 23:34:07 +0300

    [nix-maid] refactor modules to use explicit pkgs references

--- modules/user/nix-maid/text.nix
+++ modules/user/nix-maid/text.nix
@@ -5,9 +5,9 @@
   ...
 }:
 lib.mkIf (config.features.text.tex.enable or false) {
-  environment.systemPackages = with pkgs; [
-    rubber # Automated system for building LaTeX documents
-    (texlive.combined.scheme-full.withPackages (ps: [
+  environment.systemPackages = [
+    pkgs.rubber # Automated system for building LaTeX documents
+    (pkgs.texlive.combined.scheme-full.withPackages (ps: [
       ps.cyrillic # Cyrillic fonts for LaTeX
       ps.cyrillic-bin # Cyrillic binaries for LaTeX
       ps.collection-langcyrillic # Cyrillic language collection
