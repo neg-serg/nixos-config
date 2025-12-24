5b81191cba887f3448a7ef366780e122ff5606a8
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-24 21:07:53 +0300

    [hardware] annotate audio and io packages

--- modules/hardware/io/pkgs.nix
+++ modules/hardware/io/pkgs.nix
@@ -1,17 +1,17 @@
 {pkgs, ...}: {
   environment.systemPackages = [
     pkgs.blktrace # block layer tracing tools
-    pkgs.dmraid # old-style RAID config utility
-    pkgs.exfat # exFAT libs
-    pkgs.exfatprogs # exFAT utilities
-    pkgs.fio # disk benchmark
-    pkgs.gptfdisk # sgdisk and friends
-    pkgs.ioping # IO latency measuring tool
-    pkgs.mtools # MS-DOS disk utilities
-    pkgs.multipath-tools # kpartx, etc. for images
-    pkgs.nvme-cli # NVMe management tools
-    pkgs.ostree # Git for OS binaries
-    pkgs.parted # CLI disk manager
-    pkgs.smartmontools # smartctl
+    pkgs.dmraid # device mapper RAID tool for "fakeRAID" controllers
+    pkgs.exfat # user-space implementation of the exFAT filesystem
+    pkgs.exfatprogs # userspace utilities for exFAT filesystems
+    pkgs.fio # flexible I/O tester for benchmarking and verification
+    pkgs.gptfdisk # text-mode partitioning tools (gdisk, sgdisk, fixparts)
+    pkgs.ioping # simple disk I/O latency monitoring tool
+    pkgs.mtools # utilities to access MS-DOS disks from Unix
+    pkgs.multipath-tools # tools for managing multi-path devices (includes kpartx)
+    pkgs.nvme-cli # NVM-Express user space tooling
+    pkgs.ostree # library and tool for content-addressed storage of OS binaries
+    pkgs.parted # GNU Partition Editor
+    pkgs.smartmontools # tools for monitoring S.M.A.R.T. data (smartctl, smartd)
   ];
 }
