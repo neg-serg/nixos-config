d1f520ac82d98e3f4ecdcf913170bf76224261d5
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-24 21:07:53 +0300

    [hardware] annotate audio and io packages

--- modules/hardware/audio/dsp/pkgs.nix
+++ modules/hardware/audio/dsp/pkgs.nix
@@ -1,10 +1,10 @@
 {pkgs, ...}: {
   environment.systemPackages = [
-    pkgs.brutefir # FIR filter
-    pkgs.camilladsp # flexible audio DSP
-    pkgs.jamesdsp # PipeWire DSP
-    pkgs.lsp-plugins # various audio plugins
-    pkgs.yabridge # VST bridge for Linux
-    pkgs.yabridgectl # VST management on Linux
+    pkgs.brutefir # digital convolution engine for audio processing
+    pkgs.camilladsp # flexible audio DSP engine for HTTP control
+    pkgs.jamesdsp # audio effect processor for PipeWire and PulseAudio
+    pkgs.lsp-plugins # Linux Studio Plugins collection
+    pkgs.yabridge # modern and transparent VST bridge
+    pkgs.yabridgectl # command-line tool for managing yabridge VSTs
   ];
 }
