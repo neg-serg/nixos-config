4f9eb3941d8a961606cfa093f4d0f3c1e4d711f4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-24 23:44:06 +0300

    [lib] implement config.lib.neg and refactor nix-maid modules

--- modules/user/nix-maid/walker.nix
+++ modules/user/nix-maid/walker.nix
@@ -1,18 +1,16 @@
 {
   pkgs,
-  lib,
   config,
-  impurity,
   ...
 }: let
   cfg = config.features.gui.walker;
   walkerRoot = ../../../files/walker;
 in
-  lib.mkIf (cfg.enable or false) {
+  config.lib.neg.mkWhen (cfg.enable or false) {
     environment.systemPackages = [pkgs.walker]; # Wayland-native application runner
 
     users.users.neg.maid.file.home = {
-      ".config/walker/config.toml".source = impurity.link (walkerRoot + /config.toml);
-      ".config/walker/themes".source = impurity.link (walkerRoot + /themes);
+      ".config/walker/config.toml".source = config.lib.neg.linkImpure (walkerRoot + /config.toml);
+      ".config/walker/themes".source = config.lib.neg.linkImpure (walkerRoot + /themes);
     };
   }
