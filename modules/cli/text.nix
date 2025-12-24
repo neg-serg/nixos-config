345c4a7a1cee1109c5059fa9343f3b7a519073c5
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-24 21:07:45 +0300

    [cli] annotate system packages

--- modules/cli/text.nix
+++ modules/cli/text.nix
@@ -1,12 +1,12 @@
 {pkgs, ...}: {
   environment.systemPackages = [
-    pkgs.choose # yet another cut/awk alternative
-    pkgs.enca # detect + reencode text
-    pkgs.grex # generate regexes from examples
-    pkgs.grc # generic text colorizer
-    pkgs.par # paragraph reformatter
-    pkgs.sad # simpler sed alternative
-    pkgs.sd # intuitive sed alternative
-    pkgs.translate-shell # translate CLI used inside menus
+    pkgs.choose # human-friendly and fast alternative to cut and (sometimes) awk
+    pkgs.enca # Extremely Naive Charset Analyser - detects and converts text file encoding
+    pkgs.grex # generate regular expressions from user-provided test cases
+    pkgs.grc # Generic Colouriser - colorizes output of commands like ping, traceroute, gcc, etc.
+    pkgs.par # paragraph reformatter for text (useful for long lines)
+    pkgs.sad # CLI tool to search and replace in files (Selective ADiting)
+    pkgs.sd # intuitive find & replace CLI (sed alternative)
+    pkgs.translate-shell # command-line translator using Google Translate, Bing Translator, etc.
   ];
 }
