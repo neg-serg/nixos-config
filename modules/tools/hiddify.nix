66ab0a085795696891598b1d630d8dd662949b6e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-25 23:02:37 +0300

    [tools] add hiddify
    - Install Hiddify via AppImage (v2.0.5) as the nixpkgs package is unmaintained.
    - Configure appimageTools wrapType2 with libsecret and libepoxy.
    - Install desktop entry and icons from AppImage contents.

---
+++ modules/tools/hiddify.nix
@@ -0,0 +1,23 @@
+{pkgs, ...}: let
+  pname = "hiddify";
+  version = "2.0.5";
+  src = pkgs.fetchurl {
+    url = "https://github.com/hiddify/hiddify-app/releases/download/v${version}/Hiddify-Linux-x64.AppImage";
+    hash = "sha256-zVwSBiKYMK0GjrUpPQrd0PaexJ4F2D9TNS/Sk8BX4BE=";
+  };
+  appimageContents = pkgs.appimageTools.extract {inherit pname version src;};
+in {
+  environment.systemPackages = [
+    (pkgs.appimageTools.wrapType2 {
+      inherit pname version src;
+      extraPkgs = pkgs: with pkgs; [libsecret libepoxy];
+      extraInstallCommands = ''
+        install -m 444 -D ${appimageContents}/hiddify.desktop $out/share/applications/hiddify.desktop
+        install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/256x256/apps/hiddify.png \
+          $out/share/icons/hicolor/256x256/apps/hiddify.png
+        substituteInPlace $out/share/applications/hiddify.desktop \
+          --replace 'Exec=AppRun' 'Exec=${pname}'
+      '';
+    })
+  ];
+}
