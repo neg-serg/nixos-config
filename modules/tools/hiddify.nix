22ab74c19debf35c2d00178b218a3b896c999be0
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-26 01:04:20 +0300

    [tools] fix hiddify dependencies
    - Added comprehensive list of common Electron/GUI dependencies to extraPkgs for Hiddify.
    - Includes libsecret, libepoxy, gtk3, glib, nss, alsa-lib, at-spi2-atk, cups, dbus, libdrm, libxkbcommon, mesa, pango, cairo, and xorg libraries.

--- modules/tools/hiddify.nix
+++ modules/tools/hiddify.nix
@@ -10,7 +10,31 @@ in {
   environment.systemPackages = [
     (pkgs.appimageTools.wrapType2 {
       inherit pname version src;
-      extraPkgs = pkgs: with pkgs; [libsecret libepoxy];
+      extraPkgs = pkgs:
+        with pkgs; [
+          libsecret
+          libepoxy
+          gtk3
+          glib
+          nss
+          nspr
+          alsa-lib
+          at-spi2-atk
+          cups
+          dbus
+          libdrm
+          libxkbcommon
+          mesa
+          pango
+          cairo
+          xorg.libX11
+          xorg.libXcomposite
+          xorg.libXdamage
+          xorg.libXext
+          xorg.libXfixes
+          xorg.libXrandr
+          xorg.libxcb
+        ];
       extraInstallCommands = ''
         install -m 444 -D ${appimageContents}/hiddify.desktop $out/share/applications/hiddify.desktop
         install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/256x256/apps/hiddify.png \
