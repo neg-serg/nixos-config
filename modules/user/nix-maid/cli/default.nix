a24f9b36bd1bd26bb47d70ee34d28348fb10627f
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-27 04:11:40 +0300

    [modules/cli] delete file-management.nix
    migrate config to default.nix and remove deprecated file

--- modules/user/nix-maid/cli/default.nix
+++ modules/user/nix-maid/cli/default.nix
@@ -1,8 +1,14 @@
-{
+{pkgs, ...}: {
   imports = [
     ./monitoring.nix
-    ./file-management.nix
+    ./yazi.nix
     ./search.nix
     ./utils.nix
   ];
+
+  environment.systemPackages = [
+    pkgs.superfile
+  ];
+
+  environment.variables.EZA_COLORS = "da=03:uu=01:gu=0:ur=0:uw=03:ux=04;38;5;24:gr=0:gx=01;38;5;24:tx=01;38;5;24;ur=00;ue=00:tr=00:tw=00:tx=00";
 }
