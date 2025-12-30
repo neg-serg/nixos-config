f5cff4fc53f3f6340cae6beb861848a352f91407
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-12-30 23:52:16 +0300

    [web] configure yandex-browser

--- modules/web/browsers.nix
+++ modules/web/browsers.nix
@@ -2,11 +2,20 @@
   lib,
   config,
   pkgs,
+  inputs,
   ...
 }: let
-  webEnabled = config.features.web.enable or false;
+  webEnabled = (config.features.web.enable or false) || (config.features.web.yandex.enable or false);
 in {
+  imports = [inputs.yandex-browser.nixosModules.system];
+
   config = lib.mkIf webEnabled {
+    programs.yandex-browser = {
+      enable = true;
+      package = "beta";
+      # extensionInstallBlocklist = [ "imjepfoebignfgmogbbghpbkbcimgfpd" ]; # "buggy" extension in beta
+    };
+
     environment.systemPackages = lib.mkAfter [
       pkgs.passff-host # native messaging host for PassFF Firefox extension
     ];
