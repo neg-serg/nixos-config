57c15cc630880773091d31617e13882e71509901
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-04 01:39:40 +0300

    [vpn] replace deprecated nekoray with throne

--- modules/system/net/vpn/xray.nix
+++ modules/system/net/vpn/xray.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   environment.systemPackages = [
     pkgs.xorg.xhost # Manage X server access from nekoray UI
-    pkgs.nekoray # GUI client for Xray/V2Ray cores
+    pkgs.throne # GUI client for Xray/V2Ray cores
   ];
 }
