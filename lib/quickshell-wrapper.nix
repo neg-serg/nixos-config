0a845920129507e6353b6a001880832e5f890b05
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-04 01:14:19 +0300

    [lib] fix shell syntax in quickshell wrapper

--- lib/quickshell-wrapper.nix
+++ lib/quickshell-wrapper.nix
@@ -25,20 +25,20 @@
       installPhase = ''
         mkdir -p "$out/bin"
         makeWrapper ${qsBin} "$out/bin/qs" \
-          --prefix QT_PLUGIN_PATH : "${pkgs.qt6.qtbase}/${pkgs.qt6.qtbase.qtPluginPrefix}" \ # Qt base plugins
-          --prefix QT_PLUGIN_PATH : "${pkgs.qt6.qt5compat}/${pkgs.qt6.qtbase.qtPluginPrefix}" \ # Qt 5 compatibility plugins
-          --prefix QT_PLUGIN_PATH : "${pkgs.kdePackages.qtwayland}/${pkgs.qt6.qtbase.qtPluginPrefix}" \ # Wayland platform plugin
-          --prefix QT_PLUGIN_PATH : "${pkgs.qt6.qtsvg}/${pkgs.qt6.qtbase.qtPluginPrefix}" \ # SVG support plugin
-          --prefix QML2_IMPORT_PATH : "${pkgs.qt6.qt5compat}/${pkgs.qt6.qtbase.qtQmlPrefix}" \ # Qt 5 compatibility QML modules
-          --prefix QML2_IMPORT_PATH : "${pkgs.qt6.qtdeclarative}/${pkgs.qt6.qtbase.qtQmlPrefix}" \ # QML declarative modules
-          --prefix QML2_IMPORT_PATH : "${pkgs.qt6.qtpositioning}/${pkgs.qt6.qtbase.qtQmlPrefix}" \ # Qt positioning modules
-          --prefix QML2_IMPORT_PATH : "${pkgs.qt6.qtsvg}/${pkgs.qt6.qtbase.qtQmlPrefix}" \ # SVG QML modules
-          --prefix QML2_IMPORT_PATH : "${pkgs.kdePackages.syntax-highlighting}/${pkgs.qt6.qtbase.qtQmlPrefix}" \ # KSyntaxHighlighting QML module
-          --prefix QT_PLUGIN_PATH : "${pkgs.qt6.qtmultimedia}/${pkgs.qt6.qtbase.qtPluginPrefix}" \ # Multimedia plugins
-          --prefix QML2_IMPORT_PATH : "${pkgs.qt6.qtmultimedia}/${pkgs.qt6.qtbase.qtQmlPrefix}" \ # Multimedia QML modules
+          --prefix QT_PLUGIN_PATH : "${pkgs.qt6.qtbase}/${pkgs.qt6.qtbase.qtPluginPrefix}" \
+          --prefix QT_PLUGIN_PATH : "${pkgs.qt6.qt5compat}/${pkgs.qt6.qtbase.qtPluginPrefix}" \
+          --prefix QT_PLUGIN_PATH : "${pkgs.kdePackages.qtwayland}/${pkgs.qt6.qtbase.qtPluginPrefix}" \
+          --prefix QT_PLUGIN_PATH : "${pkgs.qt6.qtsvg}/${pkgs.qt6.qtbase.qtPluginPrefix}" \
+          --prefix QML2_IMPORT_PATH : "${pkgs.qt6.qt5compat}/${pkgs.qt6.qtbase.qtQmlPrefix}" \
+          --prefix QML2_IMPORT_PATH : "${pkgs.qt6.qtdeclarative}/${pkgs.qt6.qtbase.qtQmlPrefix}" \
+          --prefix QML2_IMPORT_PATH : "${pkgs.qt6.qtpositioning}/${pkgs.qt6.qtbase.qtQmlPrefix}" \
+          --prefix QML2_IMPORT_PATH : "${pkgs.qt6.qtsvg}/${pkgs.qt6.qtbase.qtQmlPrefix}" \
+          --prefix QML2_IMPORT_PATH : "${pkgs.kdePackages.syntax-highlighting}/${pkgs.qt6.qtbase.qtQmlPrefix}" \
+          --prefix QT_PLUGIN_PATH : "${pkgs.qt6.qtmultimedia}/${pkgs.qt6.qtbase.qtPluginPrefix}" \
+          --prefix QML2_IMPORT_PATH : "${pkgs.qt6.qtmultimedia}/${pkgs.qt6.qtbase.qtQmlPrefix}" \
           --prefix QML2_IMPORT_PATH : "${qsQmlPath}" \
-          --prefix XDG_DATA_DIRS : "${pkgs.adwaita-icon-theme}/share" \ # system icon theme
-          --prefix XDG_DATA_DIRS : "${pkgs.hicolor-icon-theme}/share" \ # fallback icon theme
+          --prefix XDG_DATA_DIRS : "${pkgs.adwaita-icon-theme}/share" \
+          --prefix XDG_DATA_DIRS : "${pkgs.hicolor-icon-theme}/share" \
           --set QT_QPA_PLATFORM wayland \
           --set QML_XHR_ALLOW_FILE_READ 1 \
           --prefix PATH : ${qsPath}
