69036cf5fab4facf03fc5f02fd6693ec0e3959e9
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-05 20:02:30 +0300

    [web] switch surfingkeys to nix-packaged config

--- modules/user/nix-maid/web/browsing.nix
+++ modules/user/nix-maid/web/browsing.nix
@@ -2,10 +2,13 @@
   pkgs,
   lib,
   config,
+  neg,
+  impurity ? null,
   yandexBrowserProvider ? null,
   ...
 }:
 with lib; let
+  n = neg impurity;
   needYandex = (config.features.web.enable or false) && (config.features.web.yandex.enable or false);
   yandexBrowser =
     if needYandex && yandexBrowserProvider != null
@@ -17,12 +20,17 @@ in {
     ./librewolf.nix
   ];
 
-  config = {
-    assertions = [
-      {
-        assertion = (! needYandex) || (yandexBrowser != null);
-        message = "Yandex Browser requested but 'yandexBrowser' extraSpecialArg not provided in flake.nix.";
-      }
-    ];
-  };
+  config = lib.mkMerge [
+    (n.mkHomeFiles {
+      ".config/surfingkeys.js".source = "${pkgs.surfingkeys-pkg}/share/surfingkeys/surfingkeys.js";
+    })
+    {
+      assertions = [
+        {
+          assertion = (! needYandex) || (yandexBrowser != null);
+          message = "Yandex Browser requested but 'yandexBrowser' extraSpecialArg not provided in flake.nix.";
+        }
+      ];
+    }
+  ];
 }
--- packages/surfingkeys-conf/default.nix
+++ packages/surfingkeys-conf/default.nix
@@ -11,16 +11,42 @@ buildNpmPackage {
     owner = "b0o";
     repo = "surfingkeys-conf";
     rev = "master"; # Using master as it's a config repo, often ahead of releases
-    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    hash = "sha256-udcgfQczvx6ZVr2RqdXdD5u0+lZTDf+SakxGGM7Rk3Q=";
   };
 
-  npmDepsHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  npmDepsHash = "sha256-qATWB1XQaTn6ltaCuH9EbrD1WCyDy5czwTmDAIoGD+s=";
 
   dontNpmBuild = true;
 
   buildPhase = ''
     runHook preBuild
-    npm run gulp build
+    echo "export default {};" > ./src/conf.priv.js
+    cat > build.mjs <<EOF
+    import webpack from 'webpack';
+    import config from './webpack.config.js';
+    import path from 'path';
+    import { fileURLToPath } from 'url';
+
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+    config.output = {
+      path: path.join(__dirname, 'build'),
+      filename: 'surfingkeys.js',
+    };
+
+    webpack(config, (err, stats) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      if (stats.hasErrors()) {
+        console.error(stats.toString());
+        process.exit(1);
+      }
+      console.log('Build complete');
+    });
+    EOF
+    node build.mjs
     runHook postBuild
   '';
 
