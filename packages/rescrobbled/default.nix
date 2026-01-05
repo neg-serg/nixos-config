42cd099d18cba0199c11fd11c3a1397084470ef8
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-05 09:06:49 +0300

    [media] refactor rescrobbled to remote source

--- packages/rescrobbled/default.nix
+++ packages/rescrobbled/default.nix
@@ -1,23 +1,28 @@
 {
   lib,
   rustPlatform,
+  fetchFromGitHub,
   pkg-config,
   dbus,
   openssl,
 }:
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
   pname = "rescrobbled";
   version = "0.8.0";
 
-  src = /home/neg/src/rescrobbled;
-
-  cargoLock = {
-    lockFile = ./Cargo.lock;
+  src = fetchFromGitHub {
+    owner = "InputUsername";
+    repo = "rescrobbled";
+    rev = "v${version}";
+    hash = "sha256-+5BkM4L2eB54idZ6X2ESw6ERMhG5CM4AF4BMEJm3xLU=";
   };
+
+  cargoHash = "sha256-ZawdZdP87X7xMdSdZ1VJDJxz7dBGVYo+8jR8qb2Jgq8=";
 
   nativeBuildInputs = [pkg-config];
   buildInputs = [dbus openssl];
 
+  # Tests require environment specific filters which are not active in build sandbox
   doCheck = false;
 
   meta = with lib; {
