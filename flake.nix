6ca77c42df862840715b46e20644048c09df5ee4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-06 09:12:34 +0300

    [nix] Use only official cache.nixos.org as substituter

--- flake.nix
+++ flake.nix
@@ -102,28 +102,7 @@
   };
 
   nixConfig = {
-    extra-substituters = [
-      "https://0uptime.cachix.org"
-      "https://cuda-maintainers.cachix.org"
-      "https://devenv.cachix.org"
-      "https://hercules-ci.cachix.org"
-      "https://hyprland.cachix.org"
-      "https://neg-serg.cachix.org"
-      "https://nix-community.cachix.org"
-      "https://nixpkgs-unfree.cachix.org"
-      "https://numtide.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "0uptime.cachix.org-1:ctw8yknBLg9cZBdqss+5krAem0sHYdISkw/IFdRbYdE="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-      "hercules-ci.cachix.org-1:ZZeDl9Va+xe9j+KqdzoBZMFJHVQ42Uu/c/1/KMC5Lw0="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "neg-serg.cachix.org-1:MZ+xYOrDj1Uhq8GTJAg//KrS4fAPpnIvaWU/w3Qz/wo="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
-      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-    ];
+    # Only using official cache.nixos.org - no extra substituters
   };
 
   outputs = inputs @ {
--- modules/nix/caches.data.nix
+++ modules/nix/caches.data.nix
@@ -1,30 +1,13 @@
 {
   # Shared binary caches and public keys for both flake.nix (nixConfig)
   # and modules/nix/settings.nix (nix.settings).
+  # Only official NixOS cache - third-party caches disabled for reliability.
   substituters = [
     "https://cache.nixos.org/"
-    "https://0uptime.cachix.org"
-    "https://cuda-maintainers.cachix.org"
-    "https://devenv.cachix.org"
-    "https://hercules-ci.cachix.org"
-    "https://hyprland.cachix.org"
-    "https://neg-serg.cachix.org"
-    "https://nix-community.cachix.org"
-    "https://nixpkgs-unfree.cachix.org"
-    "https://numtide.cachix.org"
   ];
 
   trusted-public-keys = [
-    # Official NixOS cache (required for cache.nixos.org)
+    # Official NixOS cache
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "0uptime.cachix.org-1:ctw8yknBLg9cZBdqss+5krAem0sHYdISkw/IFdRbYdE="
-    "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-    "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-    "hercules-ci.cachix.org-1:ZZeDl9Va+xe9j+KqdzoBZMFJHVQ42Uu/c/1/KMC5Lw0="
-    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-    "neg-serg.cachix.org-1:MZ+xYOrDj1Uhq8GTJAg//KrS4fAPpnIvaWU/w3Qz/wo="
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
-    "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
   ];
 }
