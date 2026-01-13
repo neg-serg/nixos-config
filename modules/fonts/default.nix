863e099144c700ce427ddfcc3fd0699e5f33588a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-13 18:17:58 +0300

    [fonts] restore iosevka-neg configuration

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -2,17 +2,14 @@
   lib,
   config,
   pkgs,
-
+  iosevkaNeg,
   ...
 }:
 let
   guiEnabled = config.features.gui.enable or false;
 
-  iosevkaFont = pkgs.nerd-fonts.iosevka; # fallback
-  # if iosevkaInput != null && (iosevkaInput ? nerd-font) then
-  #   iosevkaInput.nerd-font
-  # else
-  #   pkgs.nerd-fonts.iosevka;
+  iosevkaFont = if iosevkaNeg ? nerd-font then iosevkaNeg.nerd-font else pkgs.nerd-fonts.iosevka;
+
   packages = [
     pkgs.pango # ensure fontconfig has Pango shaping libs for GTK
     iosevkaFont # patched Iosevka Nerd Font for terminal/UI monospace
