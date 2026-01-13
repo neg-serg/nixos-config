fe3d1b77664484971274dbddf22380167b5c651a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-13 18:06:30 +0300

    [media] add RME ADI-2/4 Pro SE PipeWire profile

--- modules/user/session/terminal.nix
+++ modules/user/session/terminal.nix
@@ -4,6 +4,6 @@
     # -- Terminal --
     pkgs.kitty # primary GUI terminal emulator
     pkgs.kitty-img # inline image helper for Kitty
-    pkgs.warp-terminal # Warp GPU-accelerated terminal with modern UI
+
   ];
 }
