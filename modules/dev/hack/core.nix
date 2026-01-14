a9ffcb98502202c7b5834172bdfb09971b1076df
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-14 14:41:22 +0300

    [fix] security: clear modules content to resolve unused variable lints

--- modules/dev/hack/core.nix
+++ modules/dev/hack/core.nix
@@ -1,28 +1,2 @@
-{
-  lib,
-  config,
-  pkgs,
-  ...
-}:
-let
-  hackLib = import ./lib.nix { inherit lib config; };
-  flags = config.features.dev.hack.core or { };
-  groups = {
-    secrets = [
-      pkgs.gitleaks # scan repos/binaries for leaked secrets
-      pkgs.git-secrets # server-side hook toolkit preventing commits w/ secrets
-    ];
-    reverse = [
-      pkgs.capstone # multi-arch disassembly engine + CLI
-    ];
-    crawl = [
-      pkgs.katana # fast web crawler for recon
-    ];
-  };
-# _packages = hackLib.filterPackages (hackLib.mkGroupPackages flags groups);
-in
-{
-  config = lib.mkIf hackLib.enabled {
-    # environment.systemPackages = lib.mkAfter packages; # Moved to devShells.pentest
-  };
+{ ... }: { # Content moved to devShells.pentest 
 }
