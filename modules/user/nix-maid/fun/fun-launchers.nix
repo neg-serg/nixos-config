6a44b6e45581b3290af66e1865734fe510de21e4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-14 15:11:09 +0300

    [diet] refactor: move lutris to flatpak

--- modules/user/nix-maid/fun/fun-launchers.nix
+++ modules/user/nix-maid/fun/fun-launchers.nix
@@ -7,7 +7,7 @@
 with lib;
 mkIf ((config.features.fun.enable or false) && (config.features.gui.enable or false)) {
   environment.systemPackages = [
-    pkgs.lutris # open-source gaming platform for Linux
+    # pkgs.lutris # open-source gaming platform for Linux
     pkgs.wineWow64Packages.full # full 32/64-bit Wine
   ];
 }
