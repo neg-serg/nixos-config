53e6432960b2f5a35783ec67c53ce001fd62d503
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-14 17:34:43 +0300

    [user] make Discord conditional on feature flag

--- modules/user/session/chat.nix
+++ modules/user/session/chat.nix
@@ -1,10 +1,16 @@
-{ pkgs, lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   environment.systemPackages = [
     pkgs.nchat # terminal-first Telegram client
     pkgs.tdl # Telegram CLI uploader/downloader
     pkgs.telegram-desktop # Telegram GUI client
-  ] ++ (lib.optionals (config.features.apps.discord.enable or false) [
+  ]
+  ++ (lib.optionals (config.features.apps.discord.enable or false) [
     pkgs.vesktop # Discord (Vencord) desktop client
   ]);
 }
