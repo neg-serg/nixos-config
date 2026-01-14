5a38c089b437d719858fa6b1abf1c2dd0fff6c4a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-14 17:33:05 +0300

    [feature] add flags for webcam, hiddify, discord (all disabled)

--- modules/features/apps.nix
+++ modules/features/apps.nix
@@ -6,7 +6,11 @@ in
 {
   options.features.apps = {
     obsidian.autostart.enable = mkBool "autostart Obsidian at GUI login (systemd user service)" false;
-    discord.system24Theme.enable = mkBool "enable the System24 Discord/Vencord theme" true;
+    discord = {
+      enable = mkBool "enable Discord (Vesktop/Vencord)" false;
+      system24Theme.enable = mkBool "enable the System24 Discord/Vencord theme" true;
+    };
+    hiddify.enable = mkBool "enable Hiddify VPN client" false;
     winapps.enable = mkBool "enable WinApps integration (KVM/libvirt Windows VM, RDP bridge)" false;
     libreoffice.enable = mkBool "enable LibreOffice (Flatpak)" true;
   };
--- modules/tools/hiddify.nix
+++ modules/tools/hiddify.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 let
   pname = "hiddify";
   version = "2.0.5";
@@ -9,40 +9,42 @@ let
   appimageContents = pkgs.appimageTools.extract { inherit pname version src; };
 in
 {
-  environment.systemPackages = [
-    (pkgs.appimageTools.wrapType2 {
-      inherit pname version src;
-      extraPkgs = pkgs': [
-        pkgs'.libsecret
-        pkgs'.libepoxy
-        pkgs'.gtk3
-        pkgs'.glib
-        pkgs'.nss
-        pkgs'.nspr
-        pkgs'.alsa-lib
-        pkgs'.at-spi2-atk
-        pkgs'.cups
-        pkgs'.dbus
-        pkgs'.libdrm
-        pkgs'.libxkbcommon
-        pkgs'.mesa
-        pkgs'.pango
-        pkgs'.cairo
-        pkgs'.xorg.libX11
-        pkgs'.xorg.libXcomposite
-        pkgs'.xorg.libXdamage
-        pkgs'.xorg.libXext
-        pkgs'.xorg.libXfixes
-        pkgs'.xorg.libXrandr
-        pkgs'.xorg.libxcb
-      ];
-      extraInstallCommands = ''
-        install -m 444 -D ${appimageContents}/hiddify.desktop $out/share/applications/hiddify.desktop
-        install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/256x256/apps/hiddify.png \
-          $out/share/icons/hicolor/256x256/apps/hiddify.png
-        substituteInPlace $out/share/applications/hiddify.desktop \
-          --replace 'Exec=AppRun' 'Exec=${pname}'
-      '';
-    })
-  ];
+  config = lib.mkIf (config.features.apps.hiddify.enable or false) {
+    environment.systemPackages = [
+      (pkgs.appimageTools.wrapType2 {
+        inherit pname version src;
+        extraPkgs = pkgs': [
+          pkgs'.libsecret
+          pkgs'.libepoxy
+          pkgs'.gtk3
+          pkgs'.glib
+          pkgs'.nss
+          pkgs'.nspr
+          pkgs'.alsa-lib
+          pkgs'.at-spi2-atk
+          pkgs'.cups
+          pkgs'.dbus
+          pkgs'.libdrm
+          pkgs'.libxkbcommon
+          pkgs'.mesa
+          pkgs'.pango
+          pkgs'.cairo
+          pkgs'.xorg.libX11
+          pkgs'.xorg.libXcomposite
+          pkgs'.xorg.libXdamage
+          pkgs'.xorg.libXext
+          pkgs'.xorg.libXfixes
+          pkgs'.xorg.libXrandr
+          pkgs'.xorg.libxcb
+        ];
+        extraInstallCommands = ''
+          install -m 444 -D ${appimageContents}/hiddify.desktop $out/share/applications/hiddify.desktop
+          install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/256x256/apps/hiddify.png \
+            $out/share/icons/hicolor/256x256/apps/hiddify.png
+          substituteInPlace $out/share/applications/hiddify.desktop \
+            --replace 'Exec=AppRun' 'Exec=${pname}'
+        '';
+      })
+    ];
+  };
 }
