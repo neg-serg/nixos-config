79321c57e6abd5b33fa66bbf4b7781778943b944
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-14 18:06:19 +0300

    [fonts] restore FiraCode Nerd Font for kitty symbols

--- modules/user/fonts.nix
+++ modules/user/fonts.nix
@@ -2,6 +2,7 @@
 {
   fonts.fontDir.enable = true; # add fontdir support for nixos
   fonts.packages = [
+    pkgs.nerd-fonts.fira-code # FiraCode Nerd Font for terminal symbols
     pkgs.material-symbols # Material Design icon font for panels/quickshell
     pkgs.oldschool-pc-font-pack # Retro PC bitmap/outline set (Px437/PxPlus collection)
     pkgs.px437-ibm-conv-e # Px437 IBM Conv pixel outline font
