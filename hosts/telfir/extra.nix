130ce90bfbdd548e495fe582e34cba057e686734
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-15 20:00:26 +0300

    [boot] restore lanzaboote

--- hosts/telfir/extra.nix
+++ hosts/telfir/extra.nix
@@ -7,8 +7,5 @@
   imports = [ (inputs.self + "/modules/diff-closures.nix") ];
   diffClosures.enable = false; # Disabled for faster deploy; use `nix store diff-closures` manually
 
-  # Workaround Rust packaging issue in lanzaboote by disabling it on this host
-  # and using systemd-boot instead. Revert once upstream is fixed.
-  boot.lanzaboote.enable = lib.mkForce false;
-  boot.loader.systemd-boot.enable = true;
+
 }
