22aba66cb3625d20e4868873951018deb5dd37b4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-15 14:08:31 +0300

    Revert "[flake] hyprland: migrate to nixpkgs version, remove flakes"
    This reverts commit 659ad38ce1931b3a38e2255b13b2e54e489638a3.

--- flake/lib.nix
+++ flake/lib.nix
@@ -5,12 +5,26 @@
 }:
 let
 
+  hyprlandOverlay =
+    system:
+    (_: prev: {
+      inherit (inputs.xdg-desktop-portal-hyprland.packages.${system}) xdg-desktop-portal-hyprland;
+      hyprlandPlugins = prev.hyprlandPlugins // {
+        # "borders-plus-plus" = inputs.hyprland-plugins.packages.${system}."borders-plus-plus";
+        # dynamic-cursors removed
+        hy3 = inputs.hy3.packages.${system}.hy3;
+      };
+    });
+  # Note: inputs.hyprland.overlays.default is applied separately in mkPkgs
+
   mkPkgs =
     system:
     import nixpkgs {
       inherit system;
       overlays = [
         ((import ../packages/overlay.nix) inputs)
+        inputs.hyprland.overlays.default
+        (hyprlandOverlay system)
       ];
       config = import ./pkgs-config.nix;
     };
--- modules/nix/hyprland.nix
+++ modules/nix/hyprland.nix
@@ -1,5 +1,21 @@
-{ ... }:
+{ inputs, ... }:
 {
-  # Hyprland now uses standard nixpkgs version
-  nixpkgs.overlays = [ ];
+  # Route Hyprland, its portal, and the hy3 plugin in nixpkgs to the flake-pinned versions
+  nixpkgs.overlays = [
+    inputs.hyprland.overlays.default # Hyprland wayland compositor overlay
+    inputs.xdg-desktop-portal-hyprland.overlays.default # XDG portal implementation for Hyprland overlay
+    (
+      _: prev:
+      let
+        inherit (prev.stdenv.hostPlatform) system;
+      in
+      {
+        hyprlandPlugins = prev.hyprlandPlugins // {
+          # "borders-plus-plus" = inputs.hyprland-plugins.packages.${system}."borders-plus-plus";
+          # dynamic-cursors removed
+          hy3 = inputs.hy3.packages.${system}.hy3;
+        };
+      }
+    )
+  ];
 }
