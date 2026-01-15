d913587413b6b752023be86b06dc875931d3f8fa
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-15 21:32:33 +0300

    [cli] remove pipe-viewer and move vrrtest/babashka to shells

--- modules/cli/media.nix
+++ modules/cli/media.nix
@@ -4,7 +4,7 @@
     pkgs.asciinema-agg # render asciinema casts to GIF/APNG
     pkgs.chafa # terminal graphics renderer
     pkgs.exiftool # EXIF inspector for screenshot helpers
-    pkgs.pipe-viewer # terminal YouTube client
+    pkgs.mpv # multimedia player
     pkgs.sox # audio swiss-army knife for CLI helpers
     pkgs.zbar # QR/barcode scanner
   ];
