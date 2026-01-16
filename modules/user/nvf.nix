a6c078b637826ec11b83ea3a45d350ddad3226b0
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-16 11:16:55 +0300

    [dev] nvf: strip heavy extraPackages to reduce closure size

--- modules/user/nvf.nix
+++ modules/user/nvf.nix
@@ -4,7 +4,7 @@
   config,
   neg,
   impurity ? null,
-  ...
+  ../..
 }:
 let
   n = neg impurity;
@@ -111,58 +111,6 @@ lib.mkIf devEnabled (
               pkgs.ripgrep # project-wide search backend
               pkgs.fd # fast file finder used by pickers
               pkgs.tree-sitter # parser generator for treesitter
-
-              # -- LSPs & Tooling --
-              pkgs.bash-language-server # Bash LSP for shell scripts
-              pkgs.neovim-remote # nvr helper for external editor integration
-              pkgs.nil # Nix LSP (fast)
-              pkgs.pylyzer # static type analyzer for Python
-              pkgs.pyright # Microsoft Pyright LSP
-              pkgs.ruff # Python formatter/linter CLI + LSP
-              pkgs.clang-tools # clangd/clang-format for C/C++
-              pkgs.lua-language-server # Lua LSP
-              pkgs.hyprls # Hyprland config LSP
-              pkgs.emmet-language-server # Emmet completions for HTML/CSS
-              pkgs.yaml-language-server # YAML LSP
-              pkgs.taplo # TOML LSP/formatter
-              pkgs.marksman # Markdown LSP
-              pkgs.typescript-language-server # TypeScript/JS LSP
-              pkgs.vscode-langservers-extracted # HTML/CSS/JSON LSP bundle
-              pkgs.qt6.qtdeclarative # qmlfmt/qmlcachegen for QML editing
-              pkgs.qt6.qttools # qmlscene/lrelease etc. for QML dev
-              pkgs.just-lsp # LSP for justfiles
-              pkgs.lemminx # XML language server
-              pkgs.awk-language-server # AWK LSP
-              pkgs.autotools-language-server # Autoconf/Automake LSP
-              pkgs.gopls # Go language server
-              pkgs.sqls # SQL language server
-              pkgs.cmake-language-server # CMake LSP
-              pkgs.dhall-lsp-server # Dhall LSP
-              pkgs.docker-compose-language-service # docker-compose schema validation
-              pkgs.dockerfile-language-server # Dockerfile LSP
-              pkgs.dot-language-server # Graphviz DOT LSP
-              pkgs.asm-lsp # Assembly language server
-              pkgs.systemd-language-server # systemd unit LSP
-              pkgs.nginx-language-server # nginx.conf language server
-              pkgs.svls # SystemVerilog LSP
-              pkgs.vhdl-ls # VHDL language server
-              pkgs.zls # Zig language server
-
-              # -- Debuggers (DAP) --
-              pkgs.delve # Go debugger
-              pkgs.netcoredbg # .NET Core debugger
-              pkgs.lldb # LLVM debugger (C/C++/Rust)
-
-              # -- Formatters --
-              pkgs.stylua # Lua formatter
-              pkgs.shfmt # Shell formatter
-              pkgs.prettierd # Faster Prettier (HTML/CSS/JS/TS/JSON/YAML)
-              pkgs.nixfmt # Nix formatter
-              pkgs.cmake-format # CMake formatter
-              pkgs.gotools # Go tools (goimports, gofmt)
-              pkgs.isort # Python import sorter
-              pkgs.black # Python formatter
-              pkgs.rustfmt # Rust formatter
             ];
             # Avoid any nvf defaults bleeding in
             globals = { };
