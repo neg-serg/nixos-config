6ad31c7d3d368c80a2602109e64b5849a33ba9b4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-17 00:34:06 +0300

    [cli] move viddy to separate dev shell

--- modules/cli/system.nix
+++ modules/cli/system.nix
@@ -17,7 +17,6 @@
     pkgs.goaccess # real-time web log analyzer and interactive viewer
     pkgs.kmon # Linux kernel management and monitoring TUI
     pkgs.lnav # Log File Navigator - advanced log viewer for terminal
-    pkgs.viddy # modern alternative to watch command with time-travel and diffs
     pkgs.zfxtop # system monitor with focus on process grouping and ZFX style
 
     # Shell enhancements
