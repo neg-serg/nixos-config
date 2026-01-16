2c99a26aa7a9242bc0e4c455123ebb8d5e81bf52
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-16 12:55:01 +0300

    [refactor] move text utils (sad, grex, enca, choose, sd) to shell aliases; remove translate-shell/grc

--- modules/cli/text.nix
+++ modules/cli/text.nix
@@ -1,13 +1,8 @@
 { pkgs, ... }:
 {
   environment.systemPackages = [
-    pkgs.choose # human-friendly and fast alternative to cut and (sometimes) awk
-    pkgs.enca # Extremely Naive Charset Analyser - detects and converts text file encoding
-    pkgs.grex # generate regular expressions from user-provided test cases
-    pkgs.grc # Generic Colouriser - colorizes output of commands like ping, traceroute, gcc, etc.
+
     pkgs.par # paragraph reformatter for text (useful for long lines)
-    pkgs.sad # CLI tool to search and replace in files (Selective ADiting)
-    pkgs.sd # intuitive find & replace CLI (sed alternative)
-    pkgs.translate-shell # command-line translator using Google Translate, Bing Translator, etc.
+
   ];
 }
