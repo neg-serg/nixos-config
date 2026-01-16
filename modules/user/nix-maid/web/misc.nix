153bd9ccaf43838937e5c7f0b68bad6775fd1eda
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-17 00:16:25 +0300

    [user/web] move neonmodem to separate dev shell

--- modules/user/nix-maid/web/misc.nix
+++ modules/user/nix-maid/web/misc.nix
@@ -8,6 +8,6 @@
   config = lib.mkIf (config.features.web.enable && config.features.web.tools.enable) {
     # Tools moved to system-wide CLI package set (modules/cli/pkgs.nix) to keep
     # them available outside the profile.
-    environment.systemPackages = [ pkgs.neonmodem ]; # TUI for Lemmy/Kbin
+
   };
 }
