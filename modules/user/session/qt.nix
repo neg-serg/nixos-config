814c99f5fee08227f5625ca5d5692753c480dbc5
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-17 20:49:39 +0300

    [qt] cleanup: remove qtdeclarative from systemPackages

--- modules/user/session/qt.nix
+++ modules/user/session/qt.nix
@@ -4,7 +4,6 @@
     pkgs.hyprland-qt-support # Qt integration helpers for Hyprland
     pkgs.hyprland-qtutils # Qt extras (hyprland-qt-helper)
     pkgs.kdePackages.qt5compat # Qt6 QtQuick bridge
-    pkgs.kdePackages.qtdeclarative # QtDeclarative (QML runtime)
     pkgs.kdePackages.qtpositioning # Qt positioning (sensors)
     pkgs.kdePackages.qtwayland # Qt Wayland plugin
     pkgs.kdePackages.syntax-highlighting # KSyntaxHighlighting for QML
