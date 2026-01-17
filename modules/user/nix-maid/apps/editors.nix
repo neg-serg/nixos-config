c7ed96bda3aaef3d42501927042933b5bac5044a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-17 15:07:44 +0300

    [devshell] move helix to dedicated shell
    Remove the Helix editor configuration from the system-wide 'editors' module and introduce a dedicated 'helix' devShell.
    This decouples the editor from the system configuration, allowing for on-demand usage via 'nix develop .#helix'.

--- modules/user/nix-maid/apps/editors.nix
+++ modules/user/nix-maid/apps/editors.nix
@@ -6,23 +6,6 @@
   impurity ? null,
   ...
 }:
-let
-  n = neg impurity;
-  devEnabled = config.features.dev.enable or false;
-
-  # Helix Config Source
-  helixSrc = ../../../../files/helix;
-in
-lib.mkIf devEnabled (
-  lib.mkMerge [
-    {
-      environment.systemPackages = [
-        pkgs.helix # A post-modern text editor
-      ];
-    }
-    (n.mkHomeFiles {
-      # Helix Configuration
-      ".config/helix".source = n.linkImpure helixSrc;
-    })
-  ]
-)
+{
+  # Helix has been moved to a separate nix shell (see flake.nix)
+}
