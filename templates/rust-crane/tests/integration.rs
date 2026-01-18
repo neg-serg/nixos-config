4499cf2e1469121780687a4a049163977db3535f
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-18 15:07:33 +0300

    [templates] remove unused templates

--- templates/rust-crane/tests/integration.rs
+++
@@ -1,9 +0,0 @@
-use app::parse_user;
-
-#[test]
-fn integration_parses_minimal_user() {
-    let u = parse_user("{\"id\": 1, \"name\": \"alice\"}").unwrap();
-    assert_eq!(u.id, 1);
-    assert_eq!(u.name, "alice");
-}
-
