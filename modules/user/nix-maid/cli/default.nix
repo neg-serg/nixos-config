e97ace17186eaacdbfce2d68d4fec7bfcb097af1
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-18 20:50:24 +0300

    [cli] remove superfile package

--- modules/user/nix-maid/cli/default.nix
+++ modules/user/nix-maid/cli/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 {
   imports = [
     ./monitoring.nix
@@ -8,7 +8,7 @@
   ];
 
   environment.systemPackages = [
-    pkgs.superfile # Modern terminal file manager with intuitive TUI
+
   ];
 
   environment.variables.EZA_COLORS = "da=03:uu=01:gu=0:ur=0:uw=03:ux=04;38;5;24:gr=0:gx=01;38;5;24:tx=01;38;5;24;ur=00;ue=00:tr=00:tw=00:tx=00";
