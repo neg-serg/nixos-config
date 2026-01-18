b4c0f491b2e5229519c4df36678885e8207de31b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-19 01:11:34 +0300

    [fonts] remove px437-ibm-conv-e

--- modules/user/fonts.nix
+++ modules/user/fonts.nix
@@ -5,6 +5,5 @@
     pkgs.nerd-fonts.fira-code # FiraCode Nerd Font for terminal symbols
     pkgs.material-symbols # Material Design icon font for panels/quickshell
     pkgs.oldschool-pc-font-pack # Retro PC bitmap/outline set (Px437/PxPlus collection)
-    pkgs.px437-ibm-conv-e # Px437 IBM Conv pixel outline font
   ];
 }
