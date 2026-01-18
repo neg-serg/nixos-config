983743e6d4ff29b8f7b3f9cff10a2853d74641f2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-18 14:13:08 +0300

    [tools] refactor: move dconf2nix to devShell

--- modules/tools/pkgs.nix
+++ modules/tools/pkgs.nix
@@ -7,8 +7,6 @@
   environment.systemPackages = [
     pkgs.nixfmt # Nix formatter
     pkgs.cached-nix-shell # nix-shell with instant startup
-    # pkgs.cachix # download pre-built binaries (BROKEN BUILD 1.9.1)
-    pkgs.dconf2nix # convert dconf to nix config
     pkgs.deadnix # scan for dead nix code
     pkgs.manix # nixos documentation
     pkgs.niv # pin dependencies
@@ -21,6 +19,5 @@
     pkgs.nvd # compare versions: nvd diff /run/current-system result
     pkgs.nix-melt # TUI for nix flake lock --update
     pkgs.statix # static analyzer for nix
-
   ];
 }
