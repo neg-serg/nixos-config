655e8a6902456054fd421b2d6aa29df097963abf
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-19 21:59:35 +0300

    [dev/pkgs] Fix rofi wrapper to include rofi-config in XDG_DATA_DIRS

--- modules/user/nix-maid/apps/gui-apps.nix
+++ modules/user/nix-maid/apps/gui-apps.nix
@@ -17,10 +17,39 @@ let
       pkgs.rofi-calc # adds calculator capability to rofi
     ];
   };
+
+  # Wrapped rofi with custom config in XDG_DATA_DIRS
+  rofiWrapped =
+    pkgs.runCommand "rofi-wrapped"
+      {
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+      }
+      ''
+        mkdir -p $out/bin
+        makeWrapper ${rofiWithPlugins}/bin/rofi $out/bin/rofi \
+          --prefix PATH : ${
+            lib.makeBinPath [
+              pkgs.gawk # awk for text processing
+              pkgs.gnused # sed for stream editing
+              pkgs.jq # JSON processor
+            ]
+          } \
+          --prefix XDG_DATA_DIRS : ${pkgs.neg.rofi-config}/share
+
+        # Copy other files from rofiWithPlugins
+        for dir in ${rofiWithPlugins}/*; do
+          if [ "$(basename "$dir")" != "bin" ]; then
+            ln -s "$dir" "$out/$(basename "$dir")"
+          fi
+        done
+      '';
 in
 {
   config = lib.mkMerge [
     {
+      # Override rofi package to use wrapped version with custom config
+      neg.rofi.package = rofiWrapped;
+
       # Systemd user services
       systemd.user.services = {
         # SwayOSD LibInput Backend
@@ -37,18 +66,6 @@ in
 
       # Packages
       environment.systemPackages = [
-        # Rofi wrapper with config and plugins
-        (pkgs.writeShellScriptBin "rofi" ''
-          export PATH="${
-            lib.makeBinPath [
-              pkgs.gawk # awk for text processing
-              pkgs.gnused # sed for stream editing
-              pkgs.jq # JSON processor
-            ]
-          }:$PATH"
-          export XDG_DATA_DIRS="${pkgs.neg.rofi-config}/share:$XDG_DATA_DIRS"
-          exec ${rofiWithPlugins}/bin/rofi "$@"
-        '')
         pkgs.neg.rofi-config # Custom scripts (launcher, powermenu)
         pkgs.swayosd # OSD for volume/brightness on Wayland
         pkgs.wallust # Color palette generator
@@ -56,6 +73,9 @@ in
       ];
     }
     (n.mkHomeFiles {
+      # Rofi configuration directory (symlink to nix store)
+      ".config/rofi".source = "${pkgs.neg.rofi-config}/share/rofi";
+
       # Handlr Config
       ".config/handlr/handlr.toml".text = ''
         enable_selector = false
