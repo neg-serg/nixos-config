9990ab2ff5609088736abed2123f2420c77d8ed6
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-25 22:48:07 +0300

    [cli] enable carapace universal completions for zsh and nu

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -1,6 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.zsh = {
     enable = true;
+    interactiveShellInit = ''
+      # Carapace completions
+      export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense'
+      zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
+      source <(${pkgs.carapace}/bin/carapace _carapace)
+    '';
   };
 }
