7f982ff6f9ce37580d681e8530abb48252eacae8
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-28 06:27:47 +0300

    [cli] Restore broot and yazi options

--- modules/features/cli.nix
+++ modules/features/cli.nix
@@ -5,6 +5,8 @@ let
 in
 {
   options.features.cli = {
+    broot.enable = mkBool "enable broot file manager and shell integration" false;
+    yazi.enable = mkBool "enable yazi terminal file manager" true;
     television.enable = mkBool "enable television (blazingly fast fuzzy finder)" true;
     zcli = {
       enable = mkBool "install zcli helper for nh-based flake switches" false;
