cab03d085064571fb2381188378358c23750767b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-29 03:38:42 +0300

    [flake] Disable Chaotic Nyx and dependent modules

--- flake.nix
+++ flake.nix
@@ -3,32 +3,90 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
-    hy3 = { url = "git+https://github.com/outfoxxed/hy3?ref=hl0.52.0"; inputs.hyprland.follows = "hyprland"; };
+    hy3 = {
+      url = "git+https://github.com/outfoxxed/hy3?ref=hl0.52.0";
+      inputs.hyprland.follows = "hyprland";
+    };
     hyprland-protocols.follows = "hyprland/hyprland-protocols";
-    hyprland = { url = "git+https://github.com/hyprwm/Hyprland?ref=v0.52.1"; inputs.nixpkgs.follows = "nixpkgs"; };
-    raise = { url = "github:neg-serg/raise"; inputs.nixpkgs.follows = "nixpkgs"; };
+    hyprland = {
+      url = "git+https://github.com/hyprwm/Hyprland?ref=v0.52.1";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    raise = {
+      url = "github:neg-serg/raise";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     xdg-desktop-portal-hyprland.follows = "hyprland/xdph";
 
     extra-container.url = "git+file:///home/neg/src/extra-container";
-    iosevka-neg = { url = "github:neg-serg/iosevka-neg"; inputs.nixpkgs.follows = "nixpkgs"; };
-    lanzaboote = { url = "github:nix-community/lanzaboote"; inputs.nixpkgs.follows = "nixpkgs"; inputs.rust-overlay.follows = "rust-overlay"; };
-    microvm = { url = "github:microvm-nix/microvm.nix"; inputs.nixpkgs.follows = "nixpkgs"; };
+    iosevka-neg = {
+      url = "github:neg-serg/iosevka-neg";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-overlay.follows = "rust-overlay";
+    };
+    microvm = {
+      url = "github:microvm-nix/microvm.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nix-flatpak.url = "github:gmodena/nix-flatpak";
     nix-maid.url = "github:viperML/nix-maid";
-    ncps = { url = "github:kalbasit/ncps"; inputs.nixpkgs.follows = "nixpkgs"; };
-    nur = { url = "github:nix-community/NUR"; inputs.nixpkgs.follows = "nixpkgs"; };
-    nyarch-assistant = { url = "github:neg-serg/nyarch-assistant"; inputs.nixpkgs.follows = "nixpkgs"; };
-    nyxt-bin = { url = "github:neg-serg/nyxt-bin"; inputs.nixpkgs.follows = "nixpkgs"; };
-    nyx = { url = "github:chaotic-cx/nyx"; inputs.nixpkgs.follows = "nixpkgs"; inputs.rust-overlay.follows = "rust-overlay"; };
-    pre-commit-hooks = { url = "github:cachix/git-hooks.nix"; inputs.nixpkgs.follows = "nixpkgs"; };
-    quickshell = { url = "git+https://git.outfoxxed.me/quickshell/quickshell"; inputs.nixpkgs.follows = "nixpkgs"; };
-    richcolors = { url = "github:neg-serg/richcolors"; inputs.nixpkgs.follows = "nixpkgs"; };
-    rsmetrx = { url = "github:neg-serg/rsmetrx"; inputs.nixpkgs.follows = "nixpkgs"; };
-    rust-overlay = { url = "github:oxalica/rust-overlay"; inputs.nixpkgs.follows = "nixpkgs"; };
-    sops-nix = { url = "github:Mic92/sops-nix"; inputs.nixpkgs.follows = "nixpkgs"; };
-    tailray = { url = "github:NotAShelf/tailray"; inputs.nixpkgs.follows = "nixpkgs"; };
-    winapps = { url = "github:winapps-org/winapps"; inputs.nixpkgs.follows = "nixpkgs"; };
-    fsread-nvim = { url = "github:neg-serg/fsread.nvim"; flake = false; };
+    ncps = {
+      url = "github:kalbasit/ncps";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nyarch-assistant = {
+      url = "github:neg-serg/nyarch-assistant";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nyxt-bin = {
+      url = "github:neg-serg/nyxt-bin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # nyx = { url = "github:chaotic-cx/nyx"; inputs.nixpkgs.follows = "nixpkgs"; inputs.rust-overlay.follows = "rust-overlay"; };
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    quickshell = {
+      url = "git+https://git.outfoxxed.me/quickshell/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    richcolors = {
+      url = "github:neg-serg/richcolors";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rsmetrx = {
+      url = "github:neg-serg/rsmetrx";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    tailray = {
+      url = "github:NotAShelf/tailray";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    winapps = {
+      url = "github:winapps-org/winapps";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fsread-nvim = {
+      url = "github:neg-serg/fsread.nvim";
+      flake = false;
+    };
     wrapper-manager.url = "github:viperML/wrapper-manager";
   };
 
@@ -46,11 +104,13 @@
   };
 
   outputs =
-    inputs@{self, nixpkgs, ...}:
+    inputs@{ self, nixpkgs, ... }:
     let
       inherit (nixpkgs) lib;
       flakeLib = import ./flake/lib.nix {
-        inputs = inputs // { inherit self; };
+        inputs = inputs // {
+          inherit self;
+        };
         inherit nixpkgs;
       };
       supportedSystems = [ "x86_64-linux" ];
@@ -58,10 +118,16 @@
       perSystem =
         system:
         import ./flake/per-system.nix {
-          inherit self inputs nixpkgs flakeLib;
+          inherit
+            self
+            inputs
+            nixpkgs
+            flakeLib
+            ;
           pkgs = sharedPackages.${system};
         } system;
-    in {
+    in
+    {
       packages = lib.genAttrs supportedSystems (s: (perSystem s).packages);
       formatter = lib.genAttrs supportedSystems (s: (perSystem s).formatter);
       checks = lib.genAttrs supportedSystems (s: (perSystem s).checks);
