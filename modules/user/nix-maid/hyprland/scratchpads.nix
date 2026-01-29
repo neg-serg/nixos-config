e9f72d74aea9e9fc579f6ac0447dd0ed6952c435
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-01-29 04:27:44 +0300

    [hypr/scratchpads] Fix Telegram scratchpad class and add comments

--- modules/user/nix-maid/hyprland/scratchpads.nix
+++ modules/user/nix-maid/hyprland/scratchpads.nix
@@ -14,7 +14,9 @@ let
       im = {
         animation = "";
         command = "flatpak run org.telegram.desktop";
-        class = "org.telegram.desktop";
+        # Use .telegram-desktop-wrapped class because the binary is wrapped,
+        # confusing the standard window matching.
+        class = ".telegram-desktop-wrapped";
         size = "30% 95%";
         position = "69% 2%";
         lazy = true;
