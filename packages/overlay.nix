50be47583bff5b926afeb3ec2ccf798b23bcad96
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-02-02 20:44:57 +0300

    [dev/pkgs] Disable libuv tests

--- packages/overlay.nix
+++ packages/overlay.nix
@@ -76,6 +76,11 @@ in
     doCheck = false;
   });
 
+  # Disable flaky libuv tests
+  libuv = finalPrev.libuv.overrideAttrs (old: {
+    doCheck = false;
+  });
+
   # Disable flaky pytest-xdist tests
   pythonPackagesExtensions = (finalPrev.pythonPackagesExtensions or [ ]) ++ [
     (python-final: python-prev: {
