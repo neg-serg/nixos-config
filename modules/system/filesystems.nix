4bd709d15dfcd8cbc79fa450b93cca0215aa0f9a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-05-12 22:55:03 +0000

    style: fix nixfmt formatting in filesystems.nix

--- modules/system/filesystems.nix
+++ modules/system/filesystems.nix
@@ -16,57 +16,111 @@ in
     "/" = {
       device = "/dev/mapper/main2-sys";
       fsType = "xfs";
-      options = [ "rw" "relatime" "lazytime" ];
+      options = [
+        "rw"
+        "relatime"
+        "lazytime"
+      ];
     };
     "/boot" = {
       device = "/dev/disk/by-uuid/C6FE-B058";
       fsType = "vfat";
-      options = [ "x-systemd.automount" "nofail" "fmask=0177" "dmask=0077" ];
+      options = [
+        "x-systemd.automount"
+        "nofail"
+        "fmask=0177"
+        "dmask=0077"
+      ];
     };
     "/zero" = {
       device = "/dev/mapper/argon-zero";
       fsType = "xfs";
-      options = [ "x-systemd.automount" "relatime" "lazytime" "rw" ];
+      options = [
+        "x-systemd.automount"
+        "relatime"
+        "lazytime"
+        "rw"
+      ];
     };
     "${homeDir}/music" = {
       device = "/zero/music";
-      options = [ "bind" "nofail" "x-systemd.automount" ];
+      options = [
+        "bind"
+        "nofail"
+        "x-systemd.automount"
+      ];
     };
     "${homeDir}/torrent" = {
       device = "/zero/torrent";
-      options = [ "bind" "nofail" "x-systemd.automount" ];
+      options = [
+        "bind"
+        "nofail"
+        "x-systemd.automount"
+      ];
     };
     "${homeDir}/vid" = {
       device = "/zero/vid";
-      options = [ "bind" "nofail" "x-systemd.automount" ];
+      options = [
+        "bind"
+        "nofail"
+        "x-systemd.automount"
+      ];
     };
     "${homeDir}/games" = {
       device = "/zero/games";
-      options = [ "bind" "nofail" "x-systemd.automount" ];
+      options = [
+        "bind"
+        "nofail"
+        "x-systemd.automount"
+      ];
     };
     "${homeDir}/doc" = {
       device = "/zero/doc";
-      options = [ "bind" "nofail" "x-systemd.automount" ];
+      options = [
+        "bind"
+        "nofail"
+        "x-systemd.automount"
+      ];
     };
     "/var/lib/flatpak" = {
       device = "/zero/flatpak";
-      options = [ "bind" "nofail" "x-systemd.automount" ];
+      options = [
+        "bind"
+        "nofail"
+        "x-systemd.automount"
+      ];
     };
     "${homeDir}/.local/mail" = {
       device = "/zero/mail";
-      options = [ "bind" "nofail" "x-systemd.automount" ];
+      options = [
+        "bind"
+        "nofail"
+        "x-systemd.automount"
+      ];
     };
     "${homeDir}/.local/share/Steam/userdata" = {
       device = "/zero/userdata_steam";
-      options = [ "bind" "nofail" "x-systemd.automount" ];
+      options = [
+        "bind"
+        "nofail"
+        "x-systemd.automount"
+      ];
     };
     "${homeDir}/.local/share/wineprefixes" = {
       device = "/zero/wineprefixes";
-      options = [ "bind" "nofail" "x-systemd.automount" ];
+      options = [
+        "bind"
+        "nofail"
+        "x-systemd.automount"
+      ];
     };
     "${homeDir}/.cache/winetricks" = {
       device = "/zero/winetricks_cache";
-      options = [ "bind" "nofail" "x-systemd.automount" ];
+      options = [
+        "bind"
+        "nofail"
+        "x-systemd.automount"
+      ];
     };
   };
 
@@ -77,6 +131,6 @@ in
   services.fstrim = lib.mkIf isTelfir { enable = true; };
 
   systemd.tmpfiles.rules = [
-    "d /boot 0700 root root - -"
+    "d /boot 0700 root root -"
   ];
 }
