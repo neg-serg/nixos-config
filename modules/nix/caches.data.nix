0fa047820c5db64dcd6c866e4c7eda9d91d73519
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-06-29 06:50:13 +0300

    [nix/cache] Add nix-community.cachix.org public cache

--- modules/nix/caches.data.nix
+++ modules/nix/caches.data.nix
@@ -4,13 +4,16 @@
   # Official NixOS cache + Russian mirrors for lower latency.
   substituters = [
     "https://ncproxy.vizqq.cc"
+    "https://nix-community.cachix.org"
     "https://cache.nixos.org/"
   ];
 
   trusted-public-keys = [
     # Official NixOS cache
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    # Determinate Systems
+    # nix-community public cache
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    # Determinate Systems (unused, kept for reference)
     "install.determinate.systems:2/bvnFWPrR6uxEXpB7XqOSykYemH8e8WoMWvoLLXpF4="
   ];
 }
