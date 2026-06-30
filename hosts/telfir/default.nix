7efb9d0c24a70fa6f630c8f2a1ab793092ef5b77
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-06-30 18:24:04 +0300

    [hosts/telfir] Clean up whitespace and comments in default.nix

--- hosts/telfir/default.nix
+++ hosts/telfir/default.nix
@@ -6,32 +6,23 @@
     ./hardware.nix
     ./networking.nix
     ./services.nix
-    # ./virtualisation/macos.nix  # Archived - MacOS VM not currently in use
     ./virtualisation/lxc.nix
   ];
 
   system.preserveFlake = false;
-
   features.cachyos.enable = true;
-
   features.web.enable = true;
   features.web.floorp.enable = false;
-
   features.hardware.amdgpu.rocm.enable = true;
   features.mail.vdirsyncer.enable = false;
-
-  # Optimization settings
   features.optimization = {
     enable = true;
   };
-
   features.net.proxy.enable = true;
   features.net.lan-proxy.enable = true;
   features.net.transparent-proxy.enable = true;
-
   features.dev.haskell.enable = false; # Disable Haskell toolchain (saves ~1GB)
   features.dev.cpp.enable = true; # Enable C++ toolchain (ccache, gcc, cmake)
-
   boot.plymouth = {
     enable = true;
     theme = "lone";
