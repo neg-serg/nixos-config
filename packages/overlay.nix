8ac6261a524b73e52f9551a2f9e3e3bb53c157e8
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-02 15:37:32 +0300

    Revert "[dev/pkgs] Switch default llvmPackages to version 22"
    This reverts the llvmPackages override. LLVM 22 breaks
    SPIRV-LLVM-Translator required by AMD GPU graphics stack.

--- packages/overlay.nix
+++ packages/overlay.nix
@@ -31,9 +31,6 @@ in
       });
     };
 
-  # Use LLVM 22 instead of default 21 (newer, fewer LLVM versions in closure)
-  llvmPackages = final.llvmPackages_22;
-
   # Python with LTO optimizations
   python3-lto = finalPrev.python3.override {
     packageOverrides = _pythonSelf: _pythonSuper: {
