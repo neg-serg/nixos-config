4180b4d7987ed568c98b7c88950c681586943db9
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-03 21:55:17 +0300

    [nix/virt] Gate libvirtd behind features.virt.libvirtd flag

--- hosts/telfir/default.nix
+++ hosts/telfir/default.nix
@@ -20,6 +20,7 @@
   features.net.lan-proxy.enable = true;
   features.net.transparent-proxy.enable = true;
   features.dev.haskell.enable = false; # Disable Haskell toolchain (saves ~1GB)
+  features.virt.libvirtd.enable = false; # Disable KVM/QEMU (not needed on this host)
   features.apps.guiAppsFull.enable = false; # Disable heavy GUI apps (GIMP, OBS)
   features.dev.cpp.enable = true; # Enable C++ toolchain (ccache, gcc, cmake)
   boot.plymouth = {
