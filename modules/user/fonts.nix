6b61b7efccc66e873170cbd8425b8e444ccefa03
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-04 19:08:19 +0300

    [feat] add SF Pro Display, Anurati, Alfa Slab One font derivations (ported from legacy Salt config)

--- modules/user/fonts.nix
+++ modules/user/fonts.nix
@@ -5,5 +5,8 @@
     pkgs.nerd-fonts.fira-code # FiraCode Nerd Font for terminal symbols
     pkgs.material-symbols # Material Design icon font for panels/quickshell
     pkgs.oldschool-pc-font-pack # Retro PC bitmap/outline set (Px437/PxPlus collection)
+    pkgs.sf-pro-display # Apple SF Pro Display (ported from legacy Salt config)
+    pkgs.anurati # Geometric display font
+    pkgs.alfa-slab-one # Bold slab-serif display font
   ];
 }
