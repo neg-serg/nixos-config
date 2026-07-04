1a6cbb18a90c077cc9ec545c23e83cc841bfa8bb
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-04 21:57:09 +0300

    [hyprland] Inline configs and wire gtkTheme substitution in hyprland.lua

--- modules/user/nix-maid/hyprland/main.nix
+++ modules/user/nix-maid/hyprland/main.nix
@@ -9,10 +9,14 @@
 }:
 let
   guiEnabled = config.features.gui.enable or false;
+  gtkTheme = config.features.gui.gtkTheme or "Flight-Dark-GTK";
 
   environment = import ./environment.nix { inherit lib pkgs; };
   services = import ./services.nix { inherit lib pkgs inputs; };
   files = import ./files.nix { inherit lib neg impurity; };
+
+  hyprlandLuaSrc = builtins.readFile ../../../../files/gui/hypr/hyprland.lua;
+  hyprlandLuaText = builtins.replaceStrings [ "@gtkTheme@" ] [ gtkTheme ] hyprlandLuaSrc;
 in
 lib.mkIf guiEnabled (
   lib.mkMerge [
@@ -26,6 +30,7 @@ lib.mkIf guiEnabled (
     (files.generateFileLinks {
       hyprlandConfText = environment.hyprlandConf;
       permissionsConfText = environment.permissionsConf;
+      inherit hyprlandLuaText;
     })
   ]
 )
