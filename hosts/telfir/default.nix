8e5457f6352dcaa9f7e29e35e04ac936c6b01845
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-05 06:43:45 +0300

    [hosts/telfir] Plymouth: switch from gray "lone" to black "black_hud" theme

--- hosts/telfir/default.nix
+++ hosts/telfir/default.nix
@@ -24,5 +24,13 @@
   features.virt.libvirtd.enable = false; # Disable KVM/QEMU (not needed on this host)
   features.apps.guiAppsFull.enable = false; # Disable heavy GUI apps (GIMP, OBS)
   features.dev.cpp.enable = true; # Enable C++ toolchain (ccache, gcc, cmake)
-  boot.plymouth.enable = false;
+  boot.plymouth = {
+    enable = true;
+    theme = "black_hud";
+    themePackages = [
+      (pkgs.adi1090x-plymouth-themes.override {
+        selected_themes = [ "black_hud" ];
+      })
+    ];
+  };
 }
