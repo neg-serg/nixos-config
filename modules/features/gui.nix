cdd787732e3db64e9006f9e2c92f62c59f5650bc
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-05 11:19:40 +0300

    [refactor/features] Centralize mkBool option helper, flatten defaults eval

--- modules/features/gui.nix
+++ modules/features/gui.nix
@@ -1,8 +1,5 @@
-{ lib, ... }:
+{ lib, mkBool, ... }:
 with lib;
-let
-  mkBool = desc: default: (lib.mkEnableOption desc) // { inherit default; };
-in
 {
   options.features.gui = {
     enable = mkBool "enable GUI stack (wayland/hyprland, quickshell, etc.)" true;
