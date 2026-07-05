75e3ea42c72ca8b2b19601f9f8fe6020d787fb73
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-05 07:57:44 +0300

    [fonts] Add Iosevka Quasi and Proportional variants (Nerd Font)

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -8,11 +8,15 @@
 let
   guiEnabled = config.features.gui.enable or false;
 
-  iosevkaFont = iosevkaNeg.nerd-font or pkgs.nerd-fonts.iosevka;
+  iosevkaNerd = iosevkaNeg.nerd-font or pkgs.nerd-fonts.iosevka;
+  iosevkaNerdQuasi = iosevkaNeg.nerd-font-quasi or iosevkaNerd;
+  iosevkaNerdProp = iosevkaNeg.nerd-font-prop or iosevkaNerd;
 
   packages = [
     pkgs.pango # ensure fontconfig has Pango shaping libs for GTK
-    iosevkaFont # patched Iosevka Nerd Font for terminal/UI monospace
+    iosevkaNerd # patched Iosevka Nerd Font for terminal/UI monospace
+    iosevkaNerdQuasi # Iosevka Quasi (QP) for GUI readability
+    iosevkaNerdProp # Iosevka Proportional for GUI text
   ];
 in
 {
