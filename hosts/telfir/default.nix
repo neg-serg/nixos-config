058168a1f062f2187091cd1e4929de30da145a50
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-05 06:12:09 +0300

    [hosts/telfir] Disable Plymouth to remove NixOS gray bootsplash

--- hosts/telfir/default.nix
+++ hosts/telfir/default.nix
@@ -24,17 +24,5 @@
   features.virt.libvirtd.enable = false; # Disable KVM/QEMU (not needed on this host)
   features.apps.guiAppsFull.enable = false; # Disable heavy GUI apps (GIMP, OBS)
   features.dev.cpp.enable = true; # Enable C++ toolchain (ccache, gcc, cmake)
-  boot.plymouth = {
-    enable = true;
-    theme = "lone";
-    themePackages = [
-      (pkgs.adi1090x-plymouth-themes.override {
-        # collection of plymouth themes
-        selected_themes = [
-          "lone"
-          "green_blocks"
-        ];
-      })
-    ];
-  };
+  boot.plymouth.enable = false;
 }
