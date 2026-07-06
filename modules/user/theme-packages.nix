ce399808437392d077c3d514733b96b3d4f286ed
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-07 02:15:50 +0300

    [gui] Remove remaining GTK packages: swayosd, portal-gtk, themes, icons

--- modules/user/theme-packages.nix
+++ modules/user/theme-packages.nix
@@ -7,18 +7,10 @@
 }:
 let
   iosevkaFont = iosevkaNeg.nerd-font or pkgs.nerd-fonts.iosevka; # fallback iosevka font
-  gtkTheme = config.features.gui.gtkTheme or "Flight-Dark-GTK";
-  gtkThemePkg = {
-    "Flight-Dark-GTK" = pkgs.flight-gtk-theme;
-    "Andromeda" = pkgs.andromeda-gtk-theme;
-  }.${gtkTheme} or pkgs.flight-gtk-theme;
   packages = [
     iosevkaFont # patched Iosevka Nerd Font for UI monospace
-    pkgs.dconf # dconf CLI to push theme keys system-wide
-    gtkThemePkg # GTK theme (selected via features.gui.gtkTheme)
-    pkgs.kdePackages.qtstyleplugin-kvantum # Qt6 Kvantum bridge for GTK-like theming
+    pkgs.kdePackages.qtstyleplugin-kvantum # Qt6 Kvantum bridge for theme consistency
     pkgs.kora-icon-theme # sharp icon pack w/ dark + light variants
-    pkgs.adwaita-icon-theme # base fallback icons for apps Kora doesn't cover
     pkgs.libsForQt5.qtstyleplugin-kvantum # Qt5 Kvantum plugin for old apps
   ];
 in
