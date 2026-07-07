e10502a3cbd3f7d70bcbf18a0749e16647a0d31f
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-07 05:27:00 +0300

    [cli/tv] Remove television module from imports and feature flags

--- modules/features/cli.nix
+++ modules/features/cli.nix
@@ -4,6 +4,5 @@ with lib;
   options.features.cli = {
     broot.enable = mkBool "enable broot file manager and shell integration" false;
     yazi.enable = mkBool "enable yazi terminal file manager" true;
-    television.enable = mkBool "enable television (blazingly fast fuzzy finder)" true;
   };
 }
