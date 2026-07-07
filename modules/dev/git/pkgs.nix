cdc90031280088988ba859cae24b165e5eff7adc
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-07 05:26:56 +0300

    [dev/git] Remove jujutsu and act packages

--- modules/dev/git/pkgs.nix
+++ modules/dev/git/pkgs.nix
@@ -11,13 +11,11 @@ let
 in
 {
   environment.systemPackages = [
-    pkgs.jujutsu # jj: a Git-compatible VCS
     pkgs.git-crypt # git-based encryption
     pkgs.git-extras # git extra commands
     pkgs.git-filter-repo # quickly rewrite git history
     pkgs.git-lfs # git extension for large files
     pkgs.git # my favorite DVCS
-    pkgs.act # run GitHub Actions locally
     pkgs.gh # GitHub CLI
     pkgs.gist # manage GitHub gists
   ]
