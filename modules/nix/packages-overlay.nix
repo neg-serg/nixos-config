10b989fd024d54cc6bdbb50eb277c647d0f144d4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-07 05:28:43 +0300

    [nix] Remove ccacheStdenv overlay from packages-overlay

--- modules/nix/packages-overlay.nix
+++ modules/nix/packages-overlay.nix
@@ -2,10 +2,5 @@
 {
   nixpkgs.overlays = [
     (import (inputs.self + "/packages/overlay.nix") inputs)
-    # Enable ccache for all local Nix builds (compiled through stdenv).
-    # Wraps gcc/clang so object files are cached in /cache.
-    (final: prev: {
-      stdenv = prev.ccacheStdenv;
-    })
   ];
 }
