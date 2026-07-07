6c87ebc3761a800479992b58d06b2c739fb29c3a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-07 05:27:05 +0300

    [web] Remove brave browser from unfree package list

--- modules/features-data/unfree/categories/browsers.nix
+++ modules/features-data/unfree/categories/browsers.nix
@@ -3,5 +3,4 @@
   "vivaldi" # Vivaldi Browser
   "microsoft-edge" # Microsoft Edge Browser
   "microsoft-edge-stable" # Microsoft Edge Browser (standard name)
-  "brave" # Brave Browser (Chromium based, some components unfree)
 ]
