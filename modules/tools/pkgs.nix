61c29b06bcfdd55556b46bb343108c3a62819780
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-07 08:30:22 +0300

    [tools] Add nix-fast-build for parallel evaluation and builds

--- modules/tools/pkgs.nix
+++ modules/tools/pkgs.nix
@@ -13,6 +13,7 @@
     pkgs.nixos-shell # create VM for current config
     pkgs.nix-output-monitor # fancy nix output (nom)
     pkgs.npins # dependency pinning (Rust, alternative to niv)
+    pkgs.nix-fast-build # parallel evaluation+building with log renderer
     pkgs.nvd # compare versions: nvd diff /run/current-system result
     pkgs.nix-melt # TUI for nix flake lock --update
     pkgs.statix # static analyzer for nix
