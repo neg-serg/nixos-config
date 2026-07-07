648acb4ceca186341ad0b3a5f4272f2f98c38e0b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-07 03:12:22 +0300

    [flatpak] Add nix-flatpak module for Flatpak package management

---
+++ modules/flatpak/default.nix
@@ -0,0 +1,29 @@
+{ config, lib, ... }:
+{
+  imports = [ ./pkgs.nix ];
+  services.flatpak = {
+    enable = true;
+    overrides = {
+      global = {
+        Environment = {
+          XCURSOR_PATH = "/run/host/user-share/icons:/run/host/share/icons"; # Fix un-themed cursor in Wayland apps
+        };
+      };
+    };
+
+    packages = [
+      {
+        appId = "md.obsidian.Obsidian";
+        origin = "flathub";
+      }
+    ];
+    update.onActivation = true;
+  };
+
+  systemd.services.flatpak-managed-install.environment = {
+    NIX_SSL_CERT_FILE = "/etc/ssl/certs/ca-bundle.crt";
+    SSL_CERT_FILE = "/etc/ssl/certs/ca-bundle.crt";
+    CURL_CA_BUNDLE = "/etc/ssl/certs/ca-bundle.crt";
+    GIT_SSL_CAINFO = "/etc/ssl/certs/ca-bundle.crt";
+  };
+}
