25f2f67b1c1aa8541e15008ab7fdd4745bbdf6fc
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-07 19:44:22 +0300

    [tools] Remove hiddify VPN client module

--- modules/features/apps.nix
+++ modules/features/apps.nix
@@ -6,7 +6,6 @@ with lib;
       enable = mkBool "enable Obsidian knowledge base app + vault" false;
     };
     obsidian.autostart.enable = mkBool "autostart Obsidian at GUI login (systemd user service)" false;
-    hiddify.enable = mkBool "enable Hiddify VPN client" false;
     winapps.enable = mkBool "enable WinApps integration (KVM/libvirt Windows VM, RDP bridge)" false;
     throne.enable = mkBool "enable Throne GUI proxy configuration manager" false;
     guiAppsFull.enable = mkBool "enable heavy GUI apps (GIMP, OBS Studio)" true;
