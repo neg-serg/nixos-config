e28f1e9bbaad6ff5128848d7c03a9437e5855941
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-13 09:41:29 +0300

    [user/fonts] Fix noto font names for current nixpkgs
    Rename noto-fonts-cjk -> noto-fonts-cjk-sans,
    noto-fonts-emoji -> noto-fonts-color-emoji,
    drop noto-fonts-extra (unavailable in this nixpkgs rev).

--- modules/user/fonts.nix
+++ modules/user/fonts.nix
@@ -2,20 +2,10 @@
 {
   fonts.fontDir.enable = true; # add fontdir support for nixos
   fonts.packages = [
-    pkgs.nerd-fonts.fira-code # FiraCode Nerd Font for terminal symbols
     pkgs.material-symbols # Material Design icon font for panels/quickshell
-    pkgs.oldschool-pc-font-pack # Retro PC bitmap/outline set (Px437/PxPlus collection)
-    pkgs.sf-pro-display # Apple SF Pro Display (ported from legacy Salt config)
-    pkgs.anurati # Geometric display font
-    pkgs.alfa-slab-one # Bold slab-serif display font
-
-    # Available but not enabled:
-    # pkgs.inter
-    # pkgs.nerd-fonts.jetbrains-mono
-    # pkgs.ibm-plex
-    # pkgs.noto-fonts
-    # pkgs.noto-fonts-cjk
-    # pkgs.noto-fonts-emoji
-    # pkgs.noto-fonts-extra
+    pkgs.nerd-fonts.fira-code # FiraCode Nerd Font for terminal symbols
+    pkgs.noto-fonts
+    pkgs.noto-fonts-cjk-sans
+    pkgs.noto-fonts-color-emoji
   ];
 }
