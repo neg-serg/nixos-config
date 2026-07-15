0cfee191397712e4a3c0c43d5c005c3eacb6f3a3
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-16 01:32:43 +0300

    [dev/audio] Drop raysession overlay (cgitb removed in Python 3.13)
    Remove the raysession patch that removed 'from cgitb import text' - the package is replaced by new-session-manager + zestbay + pw-audioshare.

---
+++ packages/zestbay/default.nix
@@ -0,0 +1,66 @@
+{ lib
+, rustPlatform
+, fetchFromGitHub
+, cmake
+, pkg-config
+, clang
+, pipewire
+, qt6
+, lilv
+, lv2
+, suil
+, libx11
+, dbus
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "zestbay";
+  version = "0.8.4";
+
+  src = fetchFromGitHub {
+    owner = "lemonxah";
+    repo = "zestbay";
+    rev = "v${version}";
+    hash = lib.fakeHash;
+  };
+
+  cargoHash = lib.fakeHash;
+
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+    clang
+    qt6.qtbase
+    qt6.qtdeclarative
+  ];
+
+  buildInputs = [
+    pipewire
+    qt6.qtbase
+    qt6.qtdeclarative
+    qt6.qtquickcontrols2
+    qt6.qtwayland
+    lilv
+    lv2
+    suil
+    libx11
+    dbus
+  ];
+
+  # CXX-Qt needs to find Qt6 via pkg-config at build time
+  QT_SELECT = "6";
+
+  meta = with lib; {
+    description = "PipeWire patchbay with LV2/VST3/CLAP plugin hosting and auto-connect rules";
+    longDescription = ''
+      A PipeWire patchbay and audio routing manager with integrated LV2, VST3,
+      and CLAP plugin hosting. Built with Rust + Qt6/QML for a fast, native
+      Linux desktop experience.
+    '';
+    homepage = "https://github.com/lemonxah/zestbay";
+    license = licenses.mit;
+    maintainers = [ ];
+    mainProgram = "zestbay";
+    platforms = platforms.linux;
+  };
+}
