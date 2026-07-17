94479bf1e617ff13170d7c5a576b24d5b11d01ea
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-17 04:48:58 +0300

    [dev/omp] Update to 17.0.1 with npmDepsFetcherVersion 2

--- packages/omp/default.nix
+++ packages/omp/default.nix
@@ -4,10 +4,11 @@
   fetchurl,
   makeWrapper,
   bun,
+  python3,
 }:
 
 let
-  version = "17.0.0";
+  version = "17.0.1";
 in
 buildNpmPackage {
   pname = "omp";
@@ -15,10 +16,11 @@ buildNpmPackage {
 
   src = fetchurl {
     url = "https://registry.npmjs.org/@oh-my-pi/pi-coding-agent/-/pi-coding-agent-${version}.tgz";
-    hash = "sha256-MXd6v2uGMajtdCTjDdpLHAoSHE89/bMIkNmVbGersJI=";
+    hash = "sha256-bJ3s6uLJW0Eb9xQr+2wDBrBC2FLfj6hoPQ3Lbvog6H4=";
   };
 
-  npmDepsHash = "sha256-zuyj+Um4ukVUeL9lD/682B7eJP66Vw/YFCpdh5xy46Q=";
+  npmDepsHash = "sha256-z1fcxkRbw1x4H9W1f9bHinrcf+34Na1/gJJej2GaUSI=";
+  npmDepsFetcherVersion = 2;
 
   dontNpmBuild = true;
   # onnxruntime-node tries to download native binaries from nuget.org in its install script
@@ -26,11 +28,25 @@ buildNpmPackage {
   # Prevent node-gyp rebuild from also triggering scripts
   npmRebuildFlags = [ "--ignore-scripts" ];
 
+  makeCacheWritable = true;
+
   nativeBuildInputs = [ makeWrapper ];
 
   postPatch = ''
     # Inject package-lock.json (not bundled in the npm tarball)
     cp ${./package-lock.json} package-lock.json
+    # Downgrade @oh-my-pi/* deps in package.json to match lockfile (17.0.0)
+    ${lib.getExe python3} -c "
+import json
+with open('package.json') as f:
+    pkg = json.load(f)
+for dep in list(pkg.get('dependencies', {})):
+    if dep.startswith('@oh-my-pi/'):
+        pkg['dependencies'][dep] = '17.0.0'
+with open('package.json', 'w') as f:
+    json.dump(pkg, f, indent=2)
+    f.write('\n')
+"
     # Patch bun version check (nixpkgs bun 1.3.13, omp wants >=1.3.14)
     sed -i 's/1\.3\.14/1.3.13/g' dist/cli.js
   '';
