2b96e803f456afeed242b3d06913bdafd68ea541
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-19 11:17:37 +0300

    [servers/avahi] Extract mDNS service definitions into module options

--- modules/servers/avahi/default.nix
+++ modules/servers/avahi/default.nix
@@ -6,12 +6,45 @@
 {
   lib,
   config,
+  inputs,
   ...
 }:
 let
   cfg = config.servicesProfiles.avahi or { enable = false; };
+
+  mkServiceXML = { name, type, port, txtRecords ? [] }:
+    let
+      txtLines = map (r: "        <txt-record>${r}</txt-record>") txtRecords;
+    in ''
+      <?xml version="1.0" standalone='no'?>
+      <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+      <service-group>
+        <name replace-wildcards="yes">%h ${name}</name>
+        <service>
+          <type>_${type}._tcp</type>
+          <port>${toString port}</port>${lib.optionalString (txtLines != []) "\n${lib.concatStringsSep "\n" txtLines}"}
+        </service>
+      </service-group>
+    '';
 in
 {
+  options.servicesProfiles.avahi.services = lib.mkOption {
+    type = lib.types.listOf (lib.types.submodule {
+      options = {
+        name = lib.mkOption { type = lib.types.str; description = "Service name"; };
+        type = lib.mkOption { type = lib.types.str; description = "Service type sans _tcp"; };
+        port = lib.mkOption { type = lib.types.port; description = "TCP port"; };
+        txtRecords = lib.mkOption {
+          type = lib.types.listOf lib.types.str;
+          default = [];
+          description = "TXT record values";
+        };
+      };
+    });
+    default = [];
+    description = "Avahi mDNS services to publish";
+  };
+
   config = lib.mkIf cfg.enable {
     services.avahi = {
       enable = true;
@@ -24,5 +57,11 @@ in
         workstation = true;
       };
     };
+
+    # Published mDNS services from structured data
+    environment.etc = lib.listToAttrs (map (svc: {
+      name = "avahi/services/${svc.name}.service";
+      value.text = mkServiceXML svc;
+    }) cfg.services);
   };
 }
