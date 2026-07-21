aabe3770621e1ec278ecd8da333176ad84bfb1a3
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-07-21 10:27:10 +0300

    [hosts/odin] Port hyprglass v0.7.0 to Hyprland 0.55.4 API (+ 12 sed patches)

--- modules/user/nix-maid/hyprland/environment.nix
+++ modules/user/nix-maid/hyprland/environment.nix
@@ -1,5 +1,7 @@
-{ ... }: {
+{ pkgs, ... }: {
   hyprlandConf = ''
+    plugin = ${pkgs.hyprglass}/lib/libhyprglass.so
+
     source = ~/.config/hypr/hyprland.lua
     source = ~/.config/hypr/animations/selected.conf
     source = ~/.config/hypr/local.d/*.conf
