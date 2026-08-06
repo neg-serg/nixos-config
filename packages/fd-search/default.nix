80d4814b46054ca5e635856ba8a051c287410381
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-08-07 01:22:28 +0300

    [vicinae/fd-search] Document rebuild steps and vendor extension source

--- packages/fd-search/default.nix
+++ packages/fd-search/default.nix
@@ -6,6 +6,11 @@ stdenv.mkDerivation {
   pname = "vicinae-fd-search";
   version = "0.1.0";
 
+  # Bundled extension (fd-search.js + package.json + assets). To rebuild
+  # fd-search.js from src/fd-search.tsx after editing the source:
+  #   cd files/gui/vicinae-extensions/fd-search
+  #   vici build -o <outdir>   # requires vicinae dev CLI + npm deps
+  #   cp <outdir>/fd-search.js fd-search.js
   src = ../../files/gui/vicinae-extensions/fd-search;
 
   installPhase = ''
