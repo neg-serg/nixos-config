ddb15cf5143ca14c5ed0f142919ec323660763a9
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-08-10 05:49:32 +0300

    [gui/qt] Remove Catppuccin Kvantum themes

--- modules/user/nix-maid/gui/qt.nix
+++ modules/user/nix-maid/gui/qt.nix
@@ -65,67 +65,6 @@ in
         ".config/Kvantum/KvSimplicityDark/KvSimplicityDark.svg".source =
           "${pkgs.kdePackages.qtstyleplugin-kvantum}/share/Kvantum/KvSimplicityDark/KvSimplicityDark.svg";
 
-        # Catppuccin themes
-        ".config/Kvantum/Catppuccin-Mocha-Blue/Catppuccin-Mocha-Blue.kvconfig".source = "${
-          (pkgs.catppuccin-kvantum.override {
-            variant = "mocha";
-            accent = "blue";
-          })
-        }/share/Kvantum/Catppuccin-Mocha-Blue/Catppuccin-Mocha-Blue.kvconfig";
-        ".config/Kvantum/Catppuccin-Mocha-Blue/Catppuccin-Mocha-Blue.svg".source = "${
-          (pkgs.catppuccin-kvantum.override {
-            variant = "mocha";
-            accent = "blue";
-          })
-        }/share/Kvantum/Catppuccin-Mocha-Blue/Catppuccin-Mocha-Blue.svg";
-        ".config/Kvantum/Catppuccin-Mocha-Mauve/Catppuccin-Mocha-Mauve.kvconfig".source = "${
-          (pkgs.catppuccin-kvantum.override {
-            variant = "mocha";
-            accent = "mauve";
-          })
-        }/share/Kvantum/Catppuccin-Mocha-Mauve/Catppuccin-Mocha-Mauve.kvconfig";
-        ".config/Kvantum/Catppuccin-Mocha-Mauve/Catppuccin-Mocha-Mauve.svg".source = "${
-          (pkgs.catppuccin-kvantum.override {
-            variant = "mocha";
-            accent = "mauve";
-          })
-        }/share/Kvantum/Catppuccin-Mocha-Mauve/Catppuccin-Mocha-Mauve.svg";
-        ".config/Kvantum/Catppuccin-Mocha-Lavender/Catppuccin-Mocha-Lavender.kvconfig".source = "${
-          (pkgs.catppuccin-kvantum.override {
-            variant = "mocha";
-            accent = "lavender";
-          })
-        }/share/Kvantum/Catppuccin-Mocha-Lavender/Catppuccin-Mocha-Lavender.kvconfig";
-        ".config/Kvantum/Catppuccin-Mocha-Lavender/Catppuccin-Mocha-Lavender.svg".source = "${
-          (pkgs.catppuccin-kvantum.override {
-            variant = "mocha";
-            accent = "lavender";
-          })
-        }/share/Kvantum/Catppuccin-Mocha-Lavender/Catppuccin-Mocha-Lavender.svg";
-        ".config/Kvantum/Catppuccin-Mocha-Sky/Catppuccin-Mocha-Sky.kvconfig".source = "${
-          (pkgs.catppuccin-kvantum.override {
-            variant = "mocha";
-            accent = "sky";
-          })
-        }/share/Kvantum/Catppuccin-Mocha-Sky/Catppuccin-Mocha-Sky.kvconfig";
-        ".config/Kvantum/Catppuccin-Mocha-Sky/Catppuccin-Mocha-Sky.svg".source = "${
-          (pkgs.catppuccin-kvantum.override {
-            variant = "mocha";
-            accent = "sky";
-          })
-        }/share/Kvantum/Catppuccin-Mocha-Sky/Catppuccin-Mocha-Sky.svg";
-        ".config/Kvantum/Catppuccin-Mocha-Green/Catppuccin-Mocha-Green.kvconfig".source = "${
-          (pkgs.catppuccin-kvantum.override {
-            variant = "mocha";
-            accent = "green";
-          })
-        }/share/Kvantum/Catppuccin-Mocha-Green/Catppuccin-Mocha-Green.kvconfig";
-        ".config/Kvantum/Catppuccin-Mocha-Green/Catppuccin-Mocha-Green.svg".source = "${
-          (pkgs.catppuccin-kvantum.override {
-            variant = "mocha";
-            accent = "green";
-          })
-        }/share/Kvantum/Catppuccin-Mocha-Green/Catppuccin-Mocha-Green.svg";
       })
       # Bootstrap writable kvantum.kvconfig (not a nix store symlink)
       # so KvantumManager can change the active theme via "Use this theme".
