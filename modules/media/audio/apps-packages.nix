6f664c178eb13d1913bc70e11d3caf9ff72f3475
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-08-15 23:06:07 +0300

    [media/audio] Restore nicotine-plus Soulseek client

--- modules/media/audio/apps-packages.nix
+++ modules/media/audio/apps-packages.nix
@@ -27,6 +27,7 @@ let
   ++ [
 
     # -- Network --
+    pkgs.nicotine-plus # Soulseek client
     pkgs.waves # keyboard-driven terminal music player with Soulseek/Last.fm
     pkgs.scdl # SoundCloud downloader
 
