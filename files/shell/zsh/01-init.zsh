5212040f02c175202514637fd883bad21b74a8e9
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-08-25 15:55:37 +0300

    [cli] fastfetch: use Kitty Direct logo mode (no kitten subprocess)

--- files/shell/zsh/01-init.zsh
+++ files/shell/zsh/01-init.zsh
@@ -126,7 +126,7 @@ fastfetch() {
     local logo
     logo=$(ls /etc/nixos/viz/blizzard-*.webp 2>/dev/null | shuf -n1)
     if [ -n "$logo" ]; then
-      command fastfetch --logo-type kitty-icat --logo "$logo" "$@"
+      command fastfetch --logo-type kitty --logo "$logo" "$@"
       return $?
     fi
   fi
