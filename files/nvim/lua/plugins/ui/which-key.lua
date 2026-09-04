691a868af1e558d13850863a4bca3eee15da1178
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-09-04 06:34:13 +0300

    [dev/nvim] Disable which-key temporarily to test c-letter latency

--- files/nvim/lua/plugins/ui/which-key.lua
+++ files/nvim/lua/plugins/ui/which-key.lua
@@ -1,12 +1,2 @@
-return {
-  'folke/which-key.nvim',
-  event = 'VeryLazy',
-  opts = {
-    preset = 'modern',
-    delay = 400,
-    spec = {},
-  },
-  keys = {
-    { '<leader>?', function() require('which-key').show({ global = false }) end, desc = 'Buffer Keymaps' },
-  },
-}
+-- which-key disabled temporarily (c-letter latency test)
+return {}
